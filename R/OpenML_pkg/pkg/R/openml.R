e972c3021ce5b62c6576e8bab5989cf94bfd02d7
berndbischl <bernd_bischl@gmx.net>
2012-12-14 12:26:36 +0100

    work on R pkg

--- R/OpenML_pkg/pkg/R/openml.R
+++
@@ -1,8 +0,0 @@
-library(rjson)
-library(XML)
-require(RCurl)
-library(RWeka)
-
-# x = getURL("http://expdb.cs.kuleuven.be/expdb/expdbsqljson.php?q=select%20name%20from%20algorithm")
-# fromJSON(x)
-
