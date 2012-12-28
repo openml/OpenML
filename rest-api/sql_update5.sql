304410ef86f0218598e4a46a3233b3f9dde7fd53
Jan N. van Rijn <janvanrijn@gmail.com>
2012-12-28 14:39:31 +0100

    new sql update

---
+++ rest-api/sql_update5.sql
@@ -0,0 +1,3 @@
+Alter table task_math_function rename to task_function;
+
+INSERT INTO `expdb`.`task_function` (`task_id`, `math_function_name`) VALUES ('1', 'predictive_accuracy'), ('1', 'mean_absolute_error');
