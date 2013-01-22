6c21ab48746f9dada5b478468b3b058fb3fa37fd
Luis Torgo <ltorgo@gmail.com>
2013-01-22 14:41:16 +0000

    changes to slots of data description

--- R/openML/R/OpenMLTask_Class.R
+++ R/openML/R/OpenMLTask_Class.R
@@ -19,7 +19,9 @@ setClass("OpenMLTask",
          representation(task.id="numeric",
                         task.type="character",
                         task.pars="list",
-                        task.data.set="OptionalOpenMLDataSetDescription",
+                        task.data.desc.id="numeric",
+                        task.data.desc="OptionalOpenMLDataSetDescription",
+                        task.data.splits.id="numeric",
                         task.data.splits="data.frame",
                         task.preds="list"
            ))
@@ -28,12 +30,14 @@ setClass("OpenMLTask",
 # --------------------------------------------------------------
 # constructor function
 OpenMLTask <- function(task.id,task.type,task.pars,
-                       task.data.set,task.data.splits,
+                       task.data.desc.id,task.data.desc,
+                       task.data.splits.id,task.data.splits,
                        task.preds)
 {
   new("OpenMLTask",
       task.id=task.id,task.type=task.type,task.pars=task.pars,
-      task.data.set=task.data.set,task.data.splits=task.data.splits,
+      task.data.desc.id=task.data.desc.id,task.data.desc=task.data.desc,
+      task.data.splits.id=task.data.splits.id,task.data.splits=task.data.splits,
       task.preds=task.preds)
 }
 
@@ -51,12 +55,12 @@ setMethod("show","OpenMLTask",
                 '\n\nTask Type :: ',object@task.type,'\n')
             for(i in 1:length(object@task.pars))
               cat('\t',names(object@task.pars)[i],' = ',object@task.pars[[i]],'\n')
-            if (!is.null(object@task.data.set)) {
-              cat('\nDataset :: ',object@task.data.set@name,
-                  ' (openML ID = ',object@task.data.set@id,
-                  ', version = ',object@task.data.set@version,')\n')
-              cat('\tData frame with ',nrow(object@task.data.set@data.set),
-                  ' rows and ',ncol(object@task.data.set@data.set),' columns\n')
+            if (!is.null(object@task.data.desc)) {
+              cat('\nDataset :: ',object@task.data.desc@name,
+                  ' (openML ID = ',object@task.data.desc@id,
+                  ', version = ',object@task.data.desac@version,')\n')
+              cat('\tData frame with ',nrow(object@task.data.desc@data.set),
+                  ' rows and ',ncol(object@task.data.desc@data.set),' columns\n')
             }
             cat('\nPredictions ::\n')
             cat('\tFormat = ',object@task.preds$format,'\n')
