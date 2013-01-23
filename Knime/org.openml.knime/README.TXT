6a939ca70dd76303f68916b24615c7c042ff75f5
Patrick Winter <winter@merkur48.(none)>
2013-01-23 10:19:29 +0100

    Initial commit of Knime plugin

---
+++ Knime/org.openml.knime/README.TXT
@@ -0,0 +1,7 @@
+This plugin project provides nodes and functionality as an extension for KNIME.
+
+It is released under the General Public License (GPL), Version 3 (including 
+certain additional permissions according to Sec. 7 of the GPL, see the file 
+LICENSE.TXT, which should be located in the same directory as this file). 
+If you didn't receive a copy of the license file, please contact us: 
+contact@knime.org
---
+++ Knime/org.openml.knime/src/org/openml/knime/uploader/UploadPolicies.java
@@ -0,0 +1,99 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2013
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME. The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ * 
+ * History
+ *   Sep 3, 2012 (Patrick Winter): created
+ */
+package org.openml.knime.uploader;
+
+/**
+ * Enums for upload policies.
+ * 
+ * 
+ * @author Patrick Winter, KNIME.com, Zurich, Switzerland
+ */
+enum UploadPolicies {
+
+    /**
+     * Do not upload.
+     */
+    NO("No"),
+
+    /**
+     * Upload once.
+     */
+    ONCE("Once"),
+
+    /**
+     * Always upload.
+     */
+    ALWAYS("Always");
+
+    private final String m_name;
+
+    /**
+     * @param name Name of this policy
+     */
+    UploadPolicies(final String name) {
+        m_name = name;
+    }
+
+    /**
+     * @return Name of this policy
+     */
+    String getName() {
+        return m_name;
+    }
+
+    /**
+     * @return Array of all upload policy settings
+     */
+    static String[] getAllSettings() {
+        return new String[]{NO.getName(), ONCE.getName(), ALWAYS.getName()};
+    }
+
+}
