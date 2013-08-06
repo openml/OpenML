e7ec50d4859d861407a50cf5fdd50b6959660a5f
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-06 13:53:26 +0200

    R: NAMESPACE + mini fix

--- R/openML/R/xml_helpers.R
+++ R/openML/R/xml_helpers.R
@@ -1,5 +1,5 @@
 
-xmlNs = function(doc, path, optional) {
+xmlNs <- function(doc, path, optional) {
   ns = getNodeSet(doc, path)
   if (length(ns) == 0L) {
     if (optional)
@@ -11,8 +11,8 @@ xmlNs = function(doc, path, optional) {
   }
 }
 
-xmlVal = function(doc, path, optional, fun) {
-  ns = xmlNs(doc, path, optional)
+xmlVal <- function(doc, path, optional, fun) {
+  ns <- xmlNs(doc, path, optional)
 	# path not found, also cant be no optional, otherwise exception in call before
 	if (is.null(ns))
 	  return(NULL)		
@@ -23,55 +23,55 @@ xmlVal = function(doc, path, optional, fun) {
   }
 }
 
-xmlOValS = function(doc, path) {
+xmlOValS <- function(doc, path) {
   xmlVal(doc, path, TRUE, as.character)
 }
 
-xmlOValI = function(doc, path) {
+xmlOValI <- function(doc, path) {
   xmlVal(doc, path, TRUE, as.integer)
 }
 
-xmlOValR = function(doc, path) {
+xmlOValR <- function(doc, path) {
   xmlVal(doc, path, TRUE, as.numeric)
 }
 
-xmlOValD = function(doc, path) {
+xmlOValD <- function(doc, path) {
   xmlVal(doc, path, FALSE, as.Date)
 }
 
-xmlRValS = function(doc, path) {
+xmlRValS <- function(doc, path) {
   xmlVal(doc, path, FALSE, as.character)
 }
 
-xmlRValI = function(doc, path) {
+xmlRValI <- function(doc, path) {
   xmlVal(doc, path, FALSE, as.integer)
 }
 
-xmlRValR = function(doc, path) {
+xmlRValR <- function(doc, path) {
   xmlVal(doc, path, FALSE, as.numeric)
 }
 
-xmlRValD = function(doc, path) {
+xmlRValD <- function(doc, path) {
   xmlVal(doc, path, FALSE, function(x) as.POSIXct(x, tz="CET"))
 }
 
-xmlValsMultNs = function(doc, path, fun, val) {
+xmlValsMultNs <- function(doc, path, fun, val) {
   ns = getNodeSet(doc, path)
   vapply(ns, function(x) fun(xmlValue(x)), val)
 }
 
-xmlValsMultNsS = function(doc, path, fun) {
+xmlValsMultNsS <- function(doc, path, fun) {
   xmlValsMultNs(doc, path, as.character, character(1))
 }
 
-expectXMLType = function(file, doc, type) {
+expectXMLType <- function(file, doc, type) {
   r <- xmlRoot(doc)
   rootname <- xmlName(r)
   if (rootname != type) 
     stopf("Expected to find XML type %s, not %s, in file %s", type, rootname, file)
 }
 
-isErrorXML = function(doc) {
+isErrorXML <- function(doc) {
   r <- xmlRoot(doc)
   rootname <- xmlName(r)
   if (rootname == "error") {
@@ -83,15 +83,18 @@ isErrorXML = function(doc) {
   }
 }
 
-checkAndHandleErrorXML = function(file, doc, prefix.msg) {
+checkAndHandleErrorXML <- function(file, doc, prefix.msg) {
   z = isErrorXML(doc)
   if (!is.null(z)) {
     stopf("Error in server / XML response for: %s\n%s\nFile: %s", prefix.msg, z$msg, file)
   }
 }
 
-parseXMLResponse = function(file, msg, type) {
-  doc <- xmlParse(file)
+parseXMLResponse <- function(file, msg, type) {
+  doc = try(xmlParse(file))
+  if (is.error(doc)) {
+    stopf("Error in parsing XML for type %s in file: %s", type, file)
+  }
   checkAndHandleErrorXML(file, doc, msg)
   expectXMLType(file, doc, type)
   return(doc)
