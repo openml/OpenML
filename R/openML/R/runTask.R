df0ce8b48c40003fe9e4592e76acfe8d2772f9e9
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-13 17:01:52 +0200

    R: some work on runTask

--- R/openML/R/runTask.R
+++ R/openML/R/runTask.R
@@ -9,17 +9,10 @@
 #' @param task [\code{\linkS4class{OpenMLTask}}]\cr 
 #'   An (optional) OpenML task. 
 #'   Either \code{task.id} or \code{task} is required.
-#' @param learner [\code{character(1)}]\cr 
-#'   Class of learner to solve the task. By convention, all classification learners start with 
-#'   \dQuote{classif.}. A list of all learners is available on the \code{\link[mlr]{learners}} 
-#'   help page.
-#' @param upload [\code{logical(1)}]\cr
-#'   Should the run be uploaded automatically? In this case you have to provide a valid 
-#'   session token. See \code{\link{authenticateUser}}. Default is \code{FALSE}.
-#' @param session.token [\code{character(1)}]\cr
-#'   A vaild session token returned by \code{\link{authenticateUser}}. Must be provided if 
-#'   \code{upload == TRUE}.
-#' @param featch.all [\code{logical(1)}]\cr 
+#' @param learner[\code{\link[mlr]{Learner}}]\cr 
+#'   Learner object from package mlr to solve the task. 
+#'   A list of all learners is available on the \code{\link[mlr]{learners}} help page.
+#' @param fetch.all [\code{logical(1)}]\cr 
 #'   Should not only the predictions but all of the computed information be returned? This includes
 #'   test measures in each step of the resampling procedure as well as the aggregated result. See 
 #'   \code{\link[mlr]{resample}}. Default is \code{FALSE}.
@@ -28,21 +21,14 @@
 #' @seealso \code{\linkS4class{OpenMLTask}}, \code{\link[mlr]{learners}}, 
 #'   \code{\link{authenticateUser}}, \code{\link[mlr]{resample}}
 #' @export
-runTask <- function(task.id, task, learner, upload = FALSE, session.token, fetch.all = FALSE) {
-  if(missing(task)) {
-    if(missing(task.id)) {
-      stopf("Either 'task.id' or 'task' must be specified.")
-    }
-    task <- downloadOpenMLTask(task.id)
-  }
-  mlrTask <- toMLR(task)
-  lrn <- makeLearner(learner)
-  res <- resample(lrn, mlrTask$mlr.task, mlrTask$mlr.rin, measures = mlrTask$mlr.measures)
-  
-  if(upload) {
-    print("Automatic upload is not available yet.")
-  }
-  
-  if(fetch.all) return(res)
-  else return(res$pred$data)
+runTask <- function(task, learner, fetch.all = FALSE) {
+  checkArg(task, "OpenMLTask")
+  checkArg(learner, "Learner")
+  #FIXME check that learner type corresponds to task
+  mlr.task <- toMLR(task)
+  res <- resample(learner, mlr.task$mlr.task, mlr.task$mlr.rin, measures = mlr.task$mlr.measures)
+  list(
+    openml.run.result = res$pred$data, 
+    mlr.resample.pred = res
+  )  
 }
