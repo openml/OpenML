3fa86701171a7bd89e029cc59a8163b1c64bb3da
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-20 19:24:00 +0200

    R: handle more tasks in mlr

--- R/openML/R/toMLR.R
+++ R/openML/R/toMLR.R
@@ -12,9 +12,25 @@ toMLR <- function(task) {
   data.set.desc <- task@task.data.desc
   data <- task@task.data.desc@data.set
   target <- task@task.target.features
+  
+  #FIXME some data sets have empty factor levels, mlr does not like this
+  # fix this for now by removing
+  data <- drop.levels(data)
+  
+  # FIXME: hack to convert bad feature names
+  feature.ind = which(colnames(data) %nin% target)
+  feature.names = colnames(data)[feature.ind]
+  feature.names = str_replace_all(feature.names, pattern=c("\\-"), replacement="_")  
+  feature.names = str_replace_all(feature.names, pattern=c("/"), replacement="_")  
+  colnames(data)[feature.ind] = feature.names
+  
   estim.proc <- task@task.estimation.procedure
   if (task.type == "Supervised Classification") {
     mlr.task <- makeClassifTask(data = data, target = target)
+  } else if (task.type == "Supervised Regression") {
+    mlr.task <- makeRegrTask(data = data, target = target)
+  } else {
+    stopf("Encountered currently unsupported task type: %s", task.type)
   }
   mlr.rin <- createMLRResampleInstance(estim.proc, mlr.task)
   mlr.measures <- createMLRMeasures(task@task.evaluation.measures, task.type)
@@ -28,10 +44,13 @@ createMLRResampleInstance <- function(estim.proc, mlr.task) {
   data.splits <- estim.proc@data.splits
   # FIXME : more resampling
   if (type == "cross_validation") {
+    #FIXME why is stratify TRUE here? does the server always prdoced stratified
+    # resampling for classif? check this
+    stratify = (mlr.task$task.desc$type == "classif")
     if (n.repeats == 1L)
-      mlr.rdesc <- makeResampleDesc("CV", iters = n.folds, stratify = TRUE)
+      mlr.rdesc <- makeResampleDesc("CV", iters = n.folds, stratify = stratify)
     else 
-      mlr.rdesc <- makeResampleDesc("RepCV", reps = n.repeats, folds = n.folds, stratify = TRUE)
+      mlr.rdesc <- makeResampleDesc("RepCV", reps = n.repeats, folds = n.folds, stratify = stratify)
   } else {
     stopf("Unsupported estimation procedure type: %s", type)
   }
@@ -54,27 +73,28 @@ createMLRMeasures <- function(measures, type) {
   lapply(measures, function(m) {
     if(type == "Supervised Classification") {
       switch(m, 
-             mean_absolute_error = mmce,
-             area_under_roc_curve = auc,
-             build_cpu_time = timetrain,     
-             f_measure = f1,
-             matthews_correlation_coefficient = mcc,
-             precision = ppv,
-             predictive_accuracy = acc,
-             recall = tpr,
-             stopf("Unsupported evaluation measure: %s", m)     
+        mean_absolute_error = mmce,
+        area_under_roc_curve = auc,
+        build_cpu_time = timetrain,     
+        f_measure = f1,
+        matthews_correlation_coefficient = mcc,
+        precision = ppv,
+        predictive_accuracy = acc,
+        recall = tpr,
+        stopf("Unsupported evaluation measure: %s", m)     
       )
     } else {
       switch(m, 
-             mean_absolute_error = mae,
-             area_under_roc_curve = auc,
-             build_cpu_time = timetrain,     
-             f_measure = f1,
-             matthews_correlation_coefficient = mcc,
-             precision = ppv,
-             predictive_accuracy = acc,
-             recall = tpr,
-             stopf("Unsupported evaluation measure: %s", m)     
+        root_mean_squared_error = rmse,
+        mean_absolute_error = mae,
+        area_under_roc_curve = auc,
+        build_cpu_time = timetrain,     
+        f_measure = f1,
+        matthews_correlation_coefficient = mcc,
+        precision = ppv,
+        predictive_accuracy = acc,
+        recall = tpr,
+        stopf("Unsupported evaluation measure: %s", m)     
       )
     } 
   })
