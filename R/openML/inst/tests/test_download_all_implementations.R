7f87abc3c0cd1cea13188993f2e769448c892d22
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-21 21:58:23 +0200

    R: more tests

--- R/openML/inst/tests/test_download_all_implementations.R
+++ R/openML/inst/tests/test_download_all_implementations.R
@@ -3,25 +3,23 @@ context("download all implementations")
 
 test_that("download all implementations", {
   # FIXME extend test to all implementations on openml server
-  # get them from sql
-  
-  impls = c(
-    "aBoostM1(1.24.2.3)",
-    "weka.AdditiveRegression(1.17)",
-    "weka.ADTree(1.2)",
-    "weka.AODE(1.8.2.3)",
-    "weka.ARFFLoader(1.22)",
-    "weka.AttributeSelectedClassifier(1.16.2.4)",
-    "weka.AttributeSelection(1.3)",
-    "weka.Bagging(1.31.2.2)",
-    "weka.BayesNet(1.21.2.4)",
-    "weka.BestFirst(1.29)",
-    "weka.BVDecomposeSegCVSub(1.7)"
-  )
-  
+  # get them from sql, and remove rdata in test dir
+
+  #impls = load2("impl_ids_for_test.RData")
+  #FIXME remove or define complete test as external or whatever
+  errs = c()
   for (i in seq_along(impls)) {
     id = impls[i]
     print(id)
-    impl = downloadOpenMLImplementation(id=id, show.info=TRUE)
+    res = try({
+      impl = downloadOpenMLImplementation(id=id, show.info=TRUE)
+    })
+    if (is.error(res)) {
+      errs = c(errs, id)
+    }
   }
+  #FIXME remove
+  xxx <<- errs
+  print("Errors:")
+  print(errs)
 })  
