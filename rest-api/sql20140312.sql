3ba603ea307fdf977124ef3b63d3ecc613f5780b
janvanrijn <janvanrijn@gmail.com>
2014-03-19 15:22:38 +1300

    updated sql file and run xsd

--- rest-api/sql20140312.sql
+++ rest-api/sql20140312.sql
@@ -1,23 +1,33 @@
-ALTER TABLE `dataset` CHANGE `processed` `processed` DATETIME NULL DEFAULT NULL ;
-ALTER TABLE `output_data` ADD `field` VARCHAR( 128 ) NULL DEFAULT NULL ;
-ALTER TABLE `run` ADD `processed` DATETIME NULL DEFAULT NULL AFTER `error` ;
+ALTER TABLE `dataset` CHANGE `processed` `processed` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `output_data` ADD `field` VARCHAR( 128 ) NULL DEFAULT NULL;
+ALTER TABLE `run` ADD `processed` DATETIME NULL DEFAULT NULL AFTER `error`;
 
--- CAREFULL!!! SETUP ID SHOULD BE KNOWN BEFORE INSERTING THESE VALUES
+ALTER TABLE `algorithm_setup` ADD `setup_string` VARCHAR( 256 ) NULL DEFAULT NULL;
+
+CREATE TABLE `runfile` (
+  `did` int(16) NOT NULL,
+  `source` int(16) NOT NULL,
+  `field` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `format` varchar(128) NOT NULL,
+  `file_id` int(16) NOT NULL,
+  PRIMARY KEY (`did`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `schedule` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `learner` varchar(255) NOT NULL,
-  `setup_id` int(8) NOT NULL,
-  `workbench` varchar(255) NOT NULL,
+  `sid` int(8) NOT NULL,
   `ttid` int(10) NOT NULL,
+  `workbench` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
 
 
-INSERT INTO `schedule` (`id`, `learner`, `setup_id`, `workbench`, `ttid`) VALUES
-(1, 'meta.AccuracyWeightedEnsemble -l (meta.WEKAClassifier -l (weka.classifiers.functions.SMO -C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0") -w 500) -n 10.0', 12, 'moa', 4),
-(2, 'meta.AccuracyWeightedEnsemble -l (meta.WEKAClassifier -l (weka.classifiers.trees.J48 -C 0.25 -M 2) -w 500) -n 10.0', 11, 'moa', 4),
-(3, 'meta.AccuracyWeightedEnsemble -l (meta.WEKAClassifier -l (weka.classifiers.functions.Logistic -R 1.0E-8 -M -1) -w 500) -n 10.0', 13, 'moa', 4),
-(4, 'bayes.naiveBayes', 6, 'moa', 4),
-(5, 'functions.SGD', 9, 'moa', 4),
-(6, 'trees.HoeffdingTree', 7, 'moa', 4),
-(7, 'meta.LeveragingBag -l (trees.HoeffdingTree)', 10, 'moa', 4);
+-- INSERT INTO `schedule` (`id`, `learner`, `setup_id`, `workbench`, `ttid`) VALUES
+-- (1, 12, 'moa', 4),
+-- (2, 11, 'moa', 4),
+-- (3, 13, 'moa', 4),
+-- (4,  6, 'moa', 4),
+-- (5,  9, 'moa', 4),
+-- (6,  7, 'moa', 4),
+-- (7, 10, 'moa', 4);
