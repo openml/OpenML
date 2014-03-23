82a03e332ac6c52e894bd3c3ad34ee1cfe820ecf
janvanrijn <janvanrijn@gmail.com>
2014-03-24 09:47:27 +1300

    updated dateformat

--- Java/OpenmlApiConnector/src/openml/algorithms/DateParser.java
+++ Java/OpenmlApiConnector/src/openml/algorithms/DateParser.java
@@ -1,5 +1,6 @@
 package openml.algorithms;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -7,12 +8,14 @@ import java.util.TimeZone;
 
 public class DateParser {
 
+	public static final DateFormat humanReadable = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	public static final DateFormat defaultOrder  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 	public static long mysqlDateToTimeStamp(String mysqlTime) throws ParseException {
+		DateFormat current = (DateFormat) defaultOrder.clone();
 		Calendar cal = Calendar.getInstance();
-		//Date utilDate = new Date();
-	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-	    cal.setTime(sdf.parse(mysqlTime));
+	    current.setTimeZone(TimeZone.getTimeZone("GMT"));
+	    cal.setTime(current.parse(mysqlTime));
 	    //System.out.println("Valid until: " + sdf.format(cal.getTime()));
 	    //System.out.println("Now: " + sdf.format(utilDate.getTime()));
 	    return cal.getTime().getTime();
