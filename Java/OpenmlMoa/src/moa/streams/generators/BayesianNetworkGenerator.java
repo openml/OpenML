54c9e47f675c65c5efa70cb1bb5c9f4500f0a2b8
janvanrijn <janvanrijn@gmail.com>
2014-04-26 19:05:16 +1200

    OpenmlMoa: finished BayesianNetworkGenerator to coop with numeric values

--- Java/OpenmlMoa/src/moa/streams/generators/BayesianNetworkGenerator.java
+++ Java/OpenmlMoa/src/moa/streams/generators/BayesianNetworkGenerator.java
@@ -16,6 +16,7 @@ import weka.classifiers.bayes.BayesNet;
 import weka.classifiers.bayes.net.ParentSet;
 import weka.classifiers.bayes.net.search.SearchAlgorithm;
 import weka.core.Attribute;
+import weka.core.AttributeStats;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -39,6 +40,8 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		InstanceStream {
 
 	private static final long serialVersionUID = 6466101955540024300L;
+	private static final int DISCRETIZE_BINS_NOMINAL = 3;
+	private static final int DISCRETIZE_BINS_NUMERIC = 5;
 	
 	private boolean numericAttributes;
 	private InstancesHeader streamHeader;
@@ -48,6 +51,7 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 	private BayesNet bayesianNetwork;
 	private SearchAlgorithm searchAlgorithm;
 	private Map<Integer, BinDetails[]> bindetails;
+	private Discretize discretizationFilter;
 	
 	public IntOption instanceRandomSeedOption = new IntOption(
             "instanceRandomSeed", 'i',
@@ -106,7 +110,8 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		Estimator[][] m_Distributions = bayesianNetwork.getDistributions();
 		boolean[] attributesSet = new boolean[sourceDataDiscretized.numAttributes()];
 		boolean allAttribuesSet = false;
-		double[] result = new double[sourceDataDiscretized.numAttributes()];
+		double[] nominalValue = new double[sourceDataDiscretized.numAttributes()];
+		double[] usedValue = new double[sourceDataDiscretized.numAttributes()];
 		Instance inst = new DenseInstance(getHeader().numAttributes());
 		inst.setDataset(getHeader());
 		
@@ -122,25 +127,37 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 					int att_idx = m_ParentSets[iAttribute].getParent( iParent );
 					int nParent = bayesianNetwork.getParentSet(iAttribute).getParent(iParent);
 					
-					iCPT = iCPT * sourceDataDiscretized.attribute(nParent).numValues() + (int) result[nParent]; // TODO: (BUG) we still need to keep track of a nominal result and a numerical result !
+					iCPT = iCPT * sourceDataDiscretized.attribute(nParent).numValues() + (int) nominalValue[nParent]; 
 					allParentsSet &= attributesSet[att_idx]; // bit shift: If attributesSet[att_idx] = false, so will be allParentsSet
 				}
 				if( allParentsSet && attributesSet[iAttribute] == false ) {
 					attributesSet[iAttribute] = true;
 					if( numericAttributes && sourceDataOriginal.attribute( iAttribute ).isNumeric() ) {
-						result[iAttribute] = getNumericValueByProdBist(
+						double current = getNumericValueByProdBist(
 							m_Distributions[iAttribute][iCPT], 
 							sourceDataDiscretized.attribute(iAttribute) );
+						usedValue[iAttribute] = current;
+						try {
+							nominalValue[iAttribute] = getBin(current, discretizationFilter.getCutPoints( iAttribute ));
+						} catch( Exception e ) {
+							// FAIL SAFE, may never happen. 
+							e.printStackTrace();
+							nominalValue[iAttribute] = getNominalValueByProbDist( 
+								m_Distributions[iAttribute][iCPT], 
+								sourceDataDiscretized.attribute(iAttribute) );
+						}
 					} else {
-						result[iAttribute] = getNominalValueByProbDist( 
+						double current = getNominalValueByProbDist( 
 							m_Distributions[iAttribute][iCPT], 
-							sourceDataDiscretized.attribute(iAttribute) );		
+							sourceDataDiscretized.attribute(iAttribute) );
+						nominalValue[iAttribute] = current;
+						usedValue[iAttribute] = current;
 					}
 				}
 			}
 		}
 		for( int i = 0; i < getHeader().numAttributes(); ++i ) {
-			inst.setValue( i, result[i] );
+			inst.setValue( i, usedValue[i] );
 		}
 		
 		return inst;
@@ -161,11 +178,14 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 	        sourceDataDiscretized = new Instances( new FileReader( this.arffFileOption.getFile() ) );
 			String relationNameOriginal = sourceDataDiscretized.relationName();
 			
-			Discretize discretizeFilter = new Discretize();
-			String discretizeOptions = getDiscretizationOptions( sourceDataDiscretized );
-			if( discretizeOptions != null ) { sourceDataDiscretized = applyFilter(sourceDataDiscretized, discretizeFilter, discretizeOptions ); }
+			discretizationFilter = new Discretize();
+			String discretizeOptions = getDiscretizationOptions( 
+				sourceDataDiscretized, 
+				numericAttributes ? DISCRETIZE_BINS_NUMERIC : DISCRETIZE_BINS_NOMINAL,
+				numericAttributes ? true : false );
+			if( discretizeOptions != null ) { sourceDataDiscretized = applyFilter(sourceDataDiscretized, discretizationFilter, discretizeOptions ); }
 			
-			bindetails = createBinDetails( sourceDataOriginal, sourceDataDiscretized, discretizeFilter );
+			bindetails = createBinDetails( sourceDataOriginal, sourceDataDiscretized, discretizationFilter );
 			
 			sourceDataDiscretized = applyFilter(sourceDataDiscretized, new ReplaceMissingValues(), "" );
 			sourceDataDiscretized.setClass( sourceDataDiscretized.attribute( sourceDataDiscretized.numAttributes() - 1 ) );
@@ -194,7 +214,7 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		}
 	}
 	
-	public static String getDiscretizationOptions( Instances sourceData ) {
+	public static String getDiscretizationOptions( Instances sourceData, int numBins, boolean equalFrequency ) {
 		ArrayList<Integer> variablesToDiscretize = new ArrayList<Integer>();
 		for( int i = 0; i < sourceData.numAttributes(); ++i ) {
 			if( sourceData.attribute( i ).isNumeric() ) {
@@ -204,7 +224,8 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		
 		// only discretize variables if needed
 		if( variablesToDiscretize.size() > 0 ) {
-			return "-Y -B 3 -R " + StringUtils.join( variablesToDiscretize, ',' );
+			String eqFr = equalFrequency ? "-F " : "";
+			return eqFr + "-Y -B " + numBins + " -R " + StringUtils.join( variablesToDiscretize, ',' );
 		} else {
 			return null;
 		}
@@ -226,15 +247,19 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		return att.numValues() - 1; // prevent an "off by epsilon" error.
 	}
 	
+	// Interesting observation:
+	// - When taking the weighted mean, the class observation becomes very predictable
 	private double getNumericValueByProdBist( Estimator e, Attribute att ) {
-		double total_weighted_sum = 0.0;
+		Double current = instanceRandom.nextDouble();
+		Double totalFound = 0.0;
 		for( int i = 0; i < att.numValues(); ++i ) {
-			BinDetails binStatistics = bindetails.get( att.index() )[i];
-			//System.out.println(binStatistics);
-			double current = instanceRandom.nextGaussian() * binStatistics.getStdev() + binStatistics.getMean(); 
-			total_weighted_sum += e.getProbability( i ) * current; 
+			totalFound += e.getProbability( i );
+			if( current < totalFound ) return getGaussianFromBin( bindetails.get( att.index() )[i] );
 		}
-		return 0.0; // total_weighted_sum // TODO!
+		return getGaussianFromBin( bindetails.get( att.index() )[att.numValues() - 1] ); // prevent an "off by epsilon" error.
+	}
+	private double getGaussianFromBin( BinDetails binStatistics ) {
+		return instanceRandom.nextGaussian() * binStatistics.getStdev() + binStatistics.getMean(); 
 	}
 
 	private void createWekaSearchAlgorithm(String[] options) throws Exception {
@@ -245,10 +270,11 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
     }
 
 	@SuppressWarnings("unchecked")
-	private Map<Integer, BinDetails[]> createBinDetails( Instances datasetOriginal, Instances datasetDiscretized, Discretize discretizationFilter ) {
+	private static Map<Integer, BinDetails[]> createBinDetails( Instances datasetOriginal, Instances datasetDiscretized, Discretize discretizationFilter ) {
 		Map<Integer, BinDetails[]> result = new HashMap<Integer, BinDetails[]>();
 		for( int iNumAttributes = 0; iNumAttributes < datasetOriginal.numAttributes(); ++iNumAttributes ) {
 			if( datasetOriginal.attribute( iNumAttributes ).isNumeric() ) { 
+				AttributeStats attributeStatistics = datasetOriginal.attributeStats( iNumAttributes );
 				double[] cutPoints = discretizationFilter.getCutPoints( iNumAttributes );
 				int numBins = cutPoints.length + 1;
 				BinDetails[] current = new BinDetails[numBins];
@@ -257,34 +283,32 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 				
 				for( int iNumInstances = 0; iNumInstances < datasetOriginal.numInstances(); ++iNumInstances ) {
 					double currentValue = datasetOriginal.instance( iNumInstances ).value( iNumAttributes );
-					if( Utils.isMissingValue( currentValue) == false ) {
-						if( currentValue <= cutPoints[0] ) {
-							values[0].add( currentValue );
-						} else if( currentValue > cutPoints[cutPoints.length-1] ) {
-							values[cutPoints.length].add( currentValue );
-						} else {
-							for( int iNumCutPoints = 1; iNumCutPoints < cutPoints.length; ++iNumCutPoints ) {
-								if( currentValue > cutPoints[iNumCutPoints-1] && currentValue <= cutPoints[iNumCutPoints] ) {
-									values[iNumCutPoints].add( currentValue );
-								} 
-							}
-						}
-					}
+					if( Utils.isMissingValue( currentValue ) ) continue;
+					try {
+						int bin = getBin( currentValue, cutPoints );
+						values[bin].add( currentValue );
+					} catch (Exception e) {	e.printStackTrace(); } // cannot happen. 
 				}
 				
-				//System.out.println( "Attribute #" + iNumAttributes + "(" + datasetOriginal.attribute( iNumAttributes ).name() + "):" );
-				//System.out.println( "CutPoints: " + Arrays.toString( cutPoints ) );
-				//int total = 0;
 				for( int i = 0; i < numBins; ++i ) {
 					Double[] population = values[i].toArray(new Double[values[i].size()]);
-					current[i] = new BinDetails( MathHelper.mean( population ), MathHelper.standard_deviation( population, true ) );
-					/*Collections.sort( values[i] );
-					System.out.println( "- Bin " + i + ": " + values[i] );
-					System.out.println( "--- " + current[i] );
-					
-					total += values[i].size();*/
+					// TODO: check on population size.
+					if( population.length > 0 ) {
+						current[i] = new BinDetails( 
+								population.length, 
+								MathHelper.mean( population ), 
+								MathHelper.standard_deviation( population, true ) );
+					} else {
+						// empty bin, pick the minimum
+						double totalWidth = attributeStatistics.numericStats.max - attributeStatistics.numericStats.min;
+						double binWidth = totalWidth / numBins;
+						
+						current[i] = new BinDetails( 
+								population.length, 
+								attributeStatistics.numericStats.min + (i * binWidth) + (0.5 * binWidth), 
+								0 );
+					}
 				}
-				//System.out.println( "Total: " + total + "\n==========" );
 				result.put( iNumAttributes, current );
 			}
 		}
@@ -292,13 +316,40 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		return result;
 	}
 	
-	private class BinDetails {
+	private static int getBin( double number, double[] cutPoints ) throws Exception {
+		double currentValue = number;
+		if( Utils.isMissingValue( currentValue) == false ) {
+			if( currentValue <= cutPoints[0] ) {
+				return 0;
+			} else if( currentValue > cutPoints[cutPoints.length-1] ) {
+				return cutPoints.length; // there is one more bin than cutPoints.length;
+			} else {
+				for( int iNumCutPoints = 1; iNumCutPoints < cutPoints.length; ++iNumCutPoints ) {
+					if( currentValue > cutPoints[iNumCutPoints-1] && currentValue <= cutPoints[iNumCutPoints] ) {
+						return iNumCutPoints;
+					} 
+				}
+				// guaranteed that we will not come here.
+				throw new Exception("Impossible exception. Number could not be fitted in any bin: " + number );
+			}
+		} else {
+			throw new Exception("Cannot bin a missing value. ");
+		}
+	}
+	
+	private static class BinDetails {
+		private final int size;
 		private final double mean;
 		private final double stdev;
 		
-		public BinDetails( double mean, double stdev ) {
+		public BinDetails( int size, double mean, double stdev ) {
+			this.size = size;
 			this.mean = mean;
 			this.stdev = stdev;
+		}
+
+		public int getSize() {
+			return size;
 		}
 
 		public double getMean() {
@@ -311,7 +362,7 @@ public class BayesianNetworkGenerator extends AbstractOptionHandler implements
 		
 		@Override
 		public String toString() {
-			return "[mean = "+getMean()+"; stdev = "+getStdev()+"]";
+			return "[size = "+getSize()+"; mean = "+getMean()+"; stdev = "+getStdev()+"]";
 		}
 	}
 }
