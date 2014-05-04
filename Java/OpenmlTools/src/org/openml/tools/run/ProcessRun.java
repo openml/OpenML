9a07cc6689ee951fc2ef2ccaa43bcf21ac19c634
janvanrijn <janvanrijn@gmail.com>
2014-05-05 06:00:28 +1200

    added tool to speed up the processing of runs

---
+++ Java/OpenmlTools/src/org/openml/tools/run/ProcessRun.java
@@ -0,0 +1,30 @@
+package org.openml.tools.run;
+
+import org.openml.apiconnector.algorithms.QueryUtils;
+import org.openml.apiconnector.io.ApiConnector;
+import org.openml.apiconnector.settings.Config;
+import org.openml.webapplication.evaluate.EvaluateRun;
+
+public class ProcessRun {
+
+	private static Config config = new Config("username = janvanrijn@gmail.com; password = Feyenoord2002; server = http://openml.liacs.nl/");
+	
+	
+	public static void main( String[] args ) {
+		
+		ApiConnector api = new ApiConnector( config.getServer() );
+		
+		for( int i = 0; i < 2; ++i ) {
+			try {
+				process(  );
+			} catch( Exception e ) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void process(  ) throws Exception {
+		
+			EvaluateRun er = new EvaluateRun(config);
+	}
+}
