6f431e155a3c50ab811d682b725c8abd40839d57
janvanrijn <janvanrijn@gmail.com>
2014-05-05 20:34:31 +1200

    bugfixes meta dataset generator

--- Java/OpenmlTools/src/org/openml/tools/dataset/CreateMetaDataset.java
+++ Java/OpenmlTools/src/org/openml/tools/dataset/CreateMetaDataset.java
@@ -44,18 +44,18 @@ public class CreateMetaDataset {
 	
 	public static void main(String[] args) throws Exception {
 		Integer[] task_ids = { 120, 121, 122, 123, 124, 125, 126, 127, 128,
-				/*129, 130, 131, 158, 159, 160, 161, 162, 163, 164, 165, 166,
+				129, 130, 131, 158, 159, 160, 161, 162, 163, 164, 165, 166,
 				167, 168, 169, 170, 171, 172,  174, 175, 176, 177, 178,
 				179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
 				191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 2056, 2126,
 				2127, 2128, 2129, 2130, 2131, 2132, 2133, 2134, 2149, 2150,
 				2151, 2152, 2153, 2154, 2155, 2156, 2157, 2158, 2159, 2160,
-				2162, 2163, 2164, 2165, 2166, 2167, 2168, 2244*/ };
+				2162, 2163, 2164, 2165, 2166, 2167, 2168, 2244 };
 		
 		Integer[] base_classifiers = { 78, 79, 22, 80, 91, 101, 108, 103, 96, 97, 105, 99, 98, 95 };
 		Integer[] meta_classifiers = { 83, 82, 92, 87, 94, 85, 86, 84, };
 //		Integer[] weka_classifiers = { 99, 101, 103, 105, 106, 108 };
-		Integer[] lb_hoeffding = { 24 };
+		Integer[] lb_hoeffding = { 83 };
 		Integer[]  all_classifiers = { };
 		
 		RunConfiguration[] configs = new RunConfiguration[4];
@@ -69,7 +69,7 @@ public class CreateMetaDataset {
 		}
 	}
 
-	public CreateMetaDataset(Integer[] task_ids, Integer[] setup_ids, boolean oneVsAll, String filename ) throws Exception {
+	public CreateMetaDataset(Integer[] task_ids, Integer[] implementation_ids, boolean oneVsAll, String filename ) throws Exception {
 		outputfile = new BufferedWriter( new FileWriter( new File( filename ) ) );
 		runs = new HashMap<Setup, Map<Integer,Double>>();
 		tasks = new HashMap<Integer, Map<Setup,Double>>();
@@ -102,8 +102,8 @@ public class CreateMetaDataset {
 			
 			if( te.getEvaluation() != null ) {
 				for( Evaluation evaluation : te.getEvaluation() ) {
-					if( setup_ids.length == 0 || Arrays.asList( setup_ids ).contains( evaluation.getImplementation_id() ) || oneVsAll ) {
-						Setup current = new Setup( evaluation.getSetup_id(), evaluation.getImplementation() );
+					if( implementation_ids.length == 0 || Arrays.asList( implementation_ids ).contains( evaluation.getImplementation_id() ) || oneVsAll ) {
+						Setup current = new Setup( evaluation.getSetup_id(), evaluation.getImplementation_id(), evaluation.getImplementation() );
 						if( runs.containsKey( current ) == false ) {
 							runs.put( current, new HashMap<Integer, Double>() ); 
 						}
@@ -131,10 +131,13 @@ public class CreateMetaDataset {
 		// now get all implementations that have run on all tasks
 		setups = allRunsDone( runs, legal_task_ids );
 		ArrayList<String> classValues = new ArrayList<String>();
+		
 		for( Setup s : setups ) {
-			if( setup_ids.length == 0 || Arrays.asList( setup_ids ).contains( s.getSetup_id() ) ) {
+			if( implementation_ids.length == 0 || Arrays.asList( implementation_ids ).contains( s.getImplementation_id() ) ) {
 				classValues.add( s.getImplementation() );
-			} // else ignore this value. 
+			} else {
+				//Conversion.log( "OK", "Create MetaDataset", "Implementation will not be added to class values: " +  s.getSetup_id() + " - " + s.getImplementation() );
+			}
 		}
 		if( oneVsAll ) { classValues.add( ONE_VS_ALL_VALUE ); }
 		
@@ -230,12 +233,14 @@ public class CreateMetaDataset {
 	}
 	
 	private class Setup {
-		private int setup_id;
-		private String implementation;
+		private final Integer setup_id;
+		private final Integer implementation_id;
+		private final String implementation;
 		
 		// creates a specific setup
-		public Setup( int setup_id, String implementation ) {
+		public Setup( Integer setup_id, Integer implementation_id, String implementation ) {
 			this.setup_id = setup_id;
+			this.implementation_id = implementation_id;
 			this.implementation = implementation;
 		}
 
@@ -245,6 +250,10 @@ public class CreateMetaDataset {
 		
 		public String getImplementation() {
 			return implementation;
+		}
+		
+		public Integer getImplementation_id() {
+			return implementation_id;
 		}
 		
 		@Override 
