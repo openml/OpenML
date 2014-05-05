4058948e945f3a309a2e36f1bf641c97c7cebb98
janvanrijn <janvanrijn@gmail.com>
2014-05-05 18:51:46 +1200

    Work on: Weka Filter and Create Meta Dataset

--- Java/OpenmlTools/src/org/openml/tools/dataset/CreateMetaDataset.java
+++ Java/OpenmlTools/src/org/openml/tools/dataset/CreateMetaDataset.java
@@ -1,5 +1,8 @@
 package org.openml.tools.dataset;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,34 +22,47 @@ import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.OptionHandler;
+import weka.core.Utils;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.RemoveUseless;
 
 public class CreateMetaDataset {
+
+	private static final int MISSING_RUNS_TRESHOLD = 4;
+	private static final String FILENAME = "meta_lb_vs_all.arff";
+	private static final String ONE_VS_ALL_VALUE = "Others";
 	
 	private final ApiConnector ac;
 	private final Map<Integer, Map<Setup, Double>> tasks;
 	private final Map<Setup, Map<Integer, Double>> runs;
 	private final Map<Integer,Integer> taskidToDatasetid;
 	private final List<Setup> setups;
-	private static final int MISSING_RUNS_TRESHOLD = 4;
+	private final BufferedWriter outputfile;
 	
+	private final Integer[] illegal_datasets = { 116, 117, 121, 128, 148, 253, 256, 272 };
 	
 	public static void main(String[] args) throws Exception {
 		Integer[] task_ids = { 120, 121, 122, 123, 124, 125, 126, 127, 128,
-				129, 130, 131, 158, 159, 160, 161, 162, 163, 164, 165, 166,
+				129, 130, 131, 158, /*159, 160, 161, 162, 163, 164, 165, 166,
 				167, 168, 169, 170, 171, 172,  174, 175, 176, 177, 178,
 				179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
 				191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 2056, 2126,
 				2127, 2128, 2129, 2130, 2131, 2132, 2133, 2134, 2149, 2150,
 				2151, 2152, 2153, 2154, 2155, 2156, 2157, 2158, 2159, 2160,
-				 2162, 2163, 2164, 2165, 2166, 2167, 2168, 2244 };
+				 2162, 2163, 2164, 2165, 2166, 2167, 2168, 2244*/ };
+		
 		Integer[] base_classifiers = { 78, 79, 22, 80, 91, 101, 108, 103, 96, 97, 105, 99, 98, 95 };
 		Integer[] meta_classifiers = { 83, 82, 92, 87, 94, 85, 86, 84, };
+		Integer[] weka_classifiers = { 99, 101, 103, 105, 106, 108 };
+		Integer[] lb_hoeffding = { 24 };
 		Integer[]  all_classifiers = { };
 		
-		new CreateMetaDataset(task_ids, meta_classifiers);
+		new CreateMetaDataset( task_ids, all_classifiers, false, FILENAME );
 	}
 
-	public CreateMetaDataset(Integer[] task_ids, Integer[] setup_ids ) throws Exception {
+	public CreateMetaDataset(Integer[] task_ids, Integer[] setup_ids, boolean oneVsAll, String filename ) throws Exception {
+		outputfile = new BufferedWriter( new FileWriter( new File( filename ) ) );
 		runs = new HashMap<Setup, Map<Integer,Double>>();
 		tasks = new HashMap<Integer, Map<Setup,Double>>();
 		taskidToDatasetid = new HashMap<Integer, Integer>();
@@ -60,15 +76,25 @@ public class CreateMetaDataset {
 		}
 		
 		// Download all task evaluations and store the setups that were involved in all of those
+		List<Integer> legal_task_ids = new ArrayList<Integer>();
 		for( Integer task_id : task_ids ) {
 			Conversion.log( "Ok", "Create MetaDataset", "Downloading Task: " + task_id );
 			TaskEvaluations te = ac.openmlTaskEvaluations( task_id );
+			
+			int did = te.getInput_data();
+			if( Arrays.asList( illegal_datasets ).contains( did ) ) {
+				Conversion.log( "WARNING", "Create MetaDataset", "Skipping Task, illegal dataset: " + task_id + "(did "+did+")" );
+				continue;
+			} else {
+				legal_task_ids.add( task_id );
+			}
+			
 			tasks.put( task_id, new HashMap<Setup, Double>() );
-			taskidToDatasetid.put( task_id, te.getInput_data() );
+			taskidToDatasetid.put( task_id, did );
 			
 			if( te.getEvaluation() != null ) {
 				for( Evaluation evaluation : te.getEvaluation() ) {
-					if( setup_ids.length == 0 || Arrays.asList( setup_ids ).contains( evaluation.getImplementation_id() ) ) {
+					if( setup_ids.length == 0 || Arrays.asList( setup_ids ).contains( evaluation.getImplementation_id() ) || oneVsAll ) {
 						Setup current = new Setup( evaluation.getSetup_id(), evaluation.getImplementation() );
 						if( runs.containsKey( current ) == false ) {
 							runs.put( current, new HashMap<Integer, Double>() ); 
@@ -89,15 +115,21 @@ public class CreateMetaDataset {
 			}
 		}
 		
+		
+		
 		// download all sensible meta data
 		String[] metaFeatures = ac.openmlDataQualityList().getQualities();
 		
 		// now get all implementations that have run on all tasks
-		setups = allRunsDone( runs, task_ids );
+		setups = allRunsDone( runs, legal_task_ids );
 		ArrayList<String> classValues = new ArrayList<String>();
 		for( Setup s : setups ) {
-			classValues.add( s.getImplementation() );
+			if( setup_ids.length == 0 || Arrays.asList( setup_ids ).contains( s.getSetup_id() ) ) {
+				classValues.add( s.getImplementation() );
+			} // else ignore this value. 
 		}
+		if( oneVsAll ) { classValues.add( ONE_VS_ALL_VALUE ); }
+		
 		Conversion.log( "OK", "Create MetaDataset", "Found the following implementations: " + classValues );
 		Attribute classAttribute = new Attribute( "class", classValues );
 		
@@ -107,15 +139,21 @@ public class CreateMetaDataset {
 		}
 		attributes.add( classAttribute ) ;
 		
-		
-		Instances instances = new Instances( "meta_dataset", attributes, task_ids.length );
+		Instances instances = new Instances( "meta_dataset", attributes, legal_task_ids.size() );
 		instances.setClassIndex( instances.numAttributes() - 1 );
 		
-		for( Integer task_id : task_ids ) {
+		for( Integer task_id : legal_task_ids ) {
 			if( tasks.get( task_id ).size() > 0 ) {
 				Instance current = new DenseInstance( instances.numAttributes() );
 				current.setDataset(instances);
-				current.setClassValue( biggest( tasks.get( task_id) ).getImplementation() );
+				String biggest = biggest( tasks.get( task_id) ).getImplementation();
+				if( classValues.contains( biggest ) ) {
+					current.setClassValue( biggest );
+				} else if( oneVsAll ) {
+					current.setClassValue( ONE_VS_ALL_VALUE );
+				} else {
+					throw new Exception("Trying to set an illegal Classvalue: Task " + task_id + ", value " + biggest );
+				}
 				Integer dataset_id = taskidToDatasetid.get( task_id );
 	
 				Conversion.log( "Ok", "Create MetaDataset", "Downloading Qualities for did: " + dataset_id );
@@ -135,9 +173,11 @@ public class CreateMetaDataset {
 			}
 		}
 		
-		// remove 
+		// remove attributes that are unused
+		instances = applyFilter( instances, new RemoveUseless(), "-M 100.0 " ); 
 		
-		System.out.println( instances );
+		outputfile.write( instances.toString() );
+		outputfile.close();
 	}
 	
 	private Setup biggest( Map<Setup,Double> results ) {
@@ -150,8 +190,8 @@ public class CreateMetaDataset {
 		return max.getKey();
 	}
 	
-	private ArrayList<Setup> allRunsDone( Map<Setup, Map<Integer,Double>> runs, Integer[] task_ids ) {
-		ArrayList<Setup> setups = new ArrayList<Setup>();
+	private List<Setup> allRunsDone( Map<Setup, Map<Integer,Double>> runs, List<Integer> task_ids ) {
+		List<Setup> setups = new ArrayList<Setup>();
 		for( Setup setup : runs.keySet() ) {
 			Map<Integer, Double> runs_participated = runs.get( setup );
 			ArrayList<Integer> missingRuns = new ArrayList<Integer>();
@@ -161,23 +201,28 @@ public class CreateMetaDataset {
 				}
 			}
 			
-			if( missingRuns.size() > 0 ) {
-				Conversion.log( "Warning", "Create MetaDataset", "Missing task for setup :" + setup + " - " + missingRuns );
-			}
-			
 			if( missingRuns.size() < MISSING_RUNS_TRESHOLD ) {
+				Conversion.log( "OK", "Create MetaDataset", "Missing task for setup :" + setup + " - " + missingRuns );
 				setups.add( setup );
 			} else {
-				Conversion.log( "Warning", "Create MetaDataset", "DROPPING setup :" + setup );
+				Conversion.log( "Warning", "Create MetaDataset", "DROPPING setup because of too many missing runs:" + setup + " (Missing runs: "+missingRuns+")" );
 			}
 		}
 		return setups;
+	}
+	
+	// TODO: should go into a real helper class. 
+	public static Instances applyFilter( Instances dataset, Filter filter, String options ) throws Exception {
+		((OptionHandler) filter).setOptions( Utils.splitOptions( options ) );
+		filter.setInputFormat(dataset);
+		return Filter.useFilter(dataset, filter);
 	}
 	
 	private class Setup {
 		private int setup_id;
 		private String implementation;
 		
+		// creates a specific setup
 		public Setup( int setup_id, String implementation ) {
 			this.setup_id = setup_id;
 			this.implementation = implementation;
--- rest-api/useful_queries.sql
+++ rest-api/useful_queries.sql
@@ -8,4 +8,13 @@ SELECT `i`.`data`, `q`.`value` AS `numInstances`,
   COUNT(*) AS `qualities` 
 FROM `data_quality_interval` `i`, `data_quality` `q` 
 WHERE `i`.`data` = `q`.`data` AND `q`.`quality` = "NumberOfInstances" 
-GROUP BY `data`, `interval_end` - `interval_start`
+GROUP BY `data`, `interval_end` - `interval_start`;
+
+
+# inspect maximum value of meta feature, grouped by dataset and interval
+SELECT `q`.`data`, `d`.`name`, `q`.`interval_start`, `q`.`interval_end`, CONVERT( SUBSTRING_INDEX( `value`, '-', -1 ), UNSIGNED INTEGER ) AS "MaxNominalAttDistinctValues"
+FROM `data_quality_interval` `q`, `dataset` `d`
+WHERE `q`.`data` = `d`.`did`
+AND `quality` = "MaxNominalAttDistinctValues"
+GROUP BY `q`.`data`, `q`.`interval_start`, `q`.`interval_end`
+ORDER BY CONVERT( SUBSTRING_INDEX( `value`, '-', -1 ) , UNSIGNED INTEGER ) DESC;
