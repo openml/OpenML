079f141d640993fb257fb3c13ffc3c62a4ed562b
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-10-04 23:23:21 +0200

    removed println

--- Java/OpenmlWebapplication/src/org/openml/webapplication/ProcessDataset.java
+++ Java/OpenmlWebapplication/src/org/openml/webapplication/ProcessDataset.java
@@ -107,7 +107,6 @@ public class ProcessDataset {
 		} catch (OutOfMemoryError oome) {
 		    // move on
 			DataFeature datafeature = new DataFeature(did, oome.getMessage() );
-			System.out.println(xstream.toXML( datafeature ));
 			File dataFeatureFile = Conversion.stringToTempFile( xstream.toXML( datafeature ), "features-error-did" + did, "xml");
 			DataFeatureUpload dfu = apiconnector.openmlDataFeatureUpload( dataFeatureFile, ash.getSessionHash() );
 			Conversion.log( "Error", "Process Dataset", "Dataset " + dfu.getDid() + " - Error: " + oome.getMessage() );
