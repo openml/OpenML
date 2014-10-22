3a0bbf12e123b8bdf4c4d5408072d588b86f735e
janvanrijn <janvanrijn@gmail.com>
2014-10-22 10:59:26 +0200

    coop with additional api features

--- Java/OpenmlTools/src/org/openml/tools/dataset/TransferDatasets.java
+++ Java/OpenmlTools/src/org/openml/tools/dataset/TransferDatasets.java
@@ -8,7 +8,9 @@ import org.openml.apiconnector.algorithms.Conversion;
 import org.openml.apiconnector.io.OpenmlConnector;
 import org.openml.apiconnector.io.ApiSessionHash;
 import org.openml.apiconnector.settings.Config;
+import org.openml.apiconnector.settings.Settings;
 import org.openml.apiconnector.xml.DataSetDescription;
+import org.openml.apiconnector.xml.UploadDataSet;
 import org.openml.apiconnector.xstream.XstreamXmlMapping;
 
 public class TransferDatasets {
@@ -24,14 +26,12 @@ public class TransferDatasets {
 	
 	public TransferDatasets() throws Exception {
 		Config config = new Config();
-		
 		if( config.getServer() != null ) {
 			apiTo = new OpenmlConnector( config.getServer() );
 		} else { 
 			apiTo = new OpenmlConnector();
 		}
 		apiFrom = new OpenmlConnector( FROM );
-		
 		XStream xstream = XstreamXmlMapping.getInstance();
 		ApiSessionHash ash = new ApiSessionHash( apiTo );
 		if( ash.set(config.getUsername(), config.getPassword() ) == false ) {
@@ -43,12 +43,13 @@ public class TransferDatasets {
 				DataSetDescription dsd = apiFrom.openmlDataDescription( i );
 				File dataset = dsd.getDataset( null );
 				dsd.unsetUrl();
+				dsd.unsetId();
 				String descriptionXML = xstream.toXML(dsd);
 				
 				File description = Conversion.stringToTempFile( descriptionXML, dsd.getName() + "_description", "xml" );
-				System.out.println( xstream.toXML(dsd)  );
-				apiTo.openmlDataUpload( description, dataset, ash.getSessionHash() );
-				System.out.println("Succes at #" + i);
+				//System.out.println( xstream.toXML(dsd)  );
+				UploadDataSet uds = apiTo.openmlDataUpload( description, dataset, ash.getSessionHash() );
+				System.out.println("Succes at #" + i + " id: " + uds.getId() );
 			} catch( Exception e ) {
 				e.printStackTrace();
 				System.out.println( "Error at #" + i + ": " + e.getMessage() );
