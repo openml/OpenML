ff211c4b05edc6c7fdc25563756862313a26a7b0
William Raynaut <william.raynaut@gmail.com>
2017-03-01 15:28:44 +0100

    Tentative parallelisation of attribute MFs computations

--- Java/OpenmlWebapplication/src/org/openml/webapplication/features/FantailConnector.java
+++ Java/OpenmlWebapplication/src/org/openml/webapplication/features/FantailConnector.java
@@ -18,6 +18,7 @@
  *  
  */
 package org.openml.webapplication.features;
+
 import com.thoughtworks.xstream.XStream;
 import org.openml.apiconnector.algorithms.Conversion;
 import org.openml.apiconnector.io.OpenmlConnector;
@@ -39,6 +40,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class FantailConnector {
 	private final Integer window_size;
@@ -47,40 +52,41 @@ public class FantailConnector {
 	private DatabaseUtils dbUtils;
 	private GlobalMetafeatures globalMetafeatures;
 
-	
 	public FantailConnector(OpenmlConnector ac, Integer dataset_id, boolean random, String priorityTag, Integer interval_size) throws Exception {
 		window_size = interval_size;
 		apiconnector = ac;
 		dbUtils = new DatabaseUtils(apiconnector);
-        globalMetafeatures = new GlobalMetafeatures(window_size);
+		globalMetafeatures = new GlobalMetafeatures(window_size);
 
-		if(dataset_id != null) {
+		if (dataset_id != null) {
 			Conversion.log("OK", "Process Dataset", "Processing dataset " + dataset_id + " on special request. ");
 			computeMetafeatures(dataset_id);
 
 		} else {
-			dataset_id = dbUtils.getDatasetId(getExpectedNumberOfMetafeatures(), AttributeMetafeatures.numberOfAttributeMetafeatures(), window_size, random, priorityTag);
-			while( dataset_id != null ) {
+			dataset_id = dbUtils.getDatasetId(getExpectedNumberOfMetafeatures(), AttributeMetafeatures.numberOfAttributeMetafeatures(), window_size, random,
+					priorityTag);
+			while (dataset_id != null) {
 				Conversion.log("OK", "Process Dataset", "Processing dataset " + dataset_id + " as obtained from database. ");
 				computeMetafeatures(dataset_id);
-				dataset_id = dbUtils.getDatasetId(getExpectedNumberOfMetafeatures(), AttributeMetafeatures.numberOfAttributeMetafeatures(), window_size, random, priorityTag);
+				dataset_id = dbUtils.getDatasetId(getExpectedNumberOfMetafeatures(), AttributeMetafeatures.numberOfAttributeMetafeatures(), window_size, random,
+						priorityTag);
 			}
 			Conversion.log("OK", "Process Dataset", "No more datasets to process. ");
 		}
 	}
 
-	private int getExpectedNumberOfMetafeatures(){
+	private int getExpectedNumberOfMetafeatures() {
 		int expectedGlobal = globalMetafeatures.getExpectedQualities();
 		return expectedGlobal;
 	}
 
 	private void computeMetafeatures(int datasetId) throws Exception {
-	    DataSetDescription dsd = dbUtils.GetDatasetDescription(datasetId);
-	    Instances dataset = dbUtils.getDataset(dsd);
-	    List<String> qualitiesAvailable = dbUtils.GetQualitiesAvailable(datasetId, window_size);
-        extractFeatures(dsd, dataset, qualitiesAvailable);
-    }
-	
+		DataSetDescription dsd = dbUtils.GetDatasetDescription(datasetId);
+		Instances dataset = dbUtils.getDataset(dsd);
+		List<String> qualitiesAvailable = dbUtils.GetQualitiesAvailable(datasetId, window_size);
+		extractFeatures(dsd, dataset, qualitiesAvailable);
+	}
+
 	private void extractFeatures(DataSetDescription dsd, Instances dataset, List<String> qualitiesAvailable) throws Exception {
 		Conversion.log("OK", "Extract Features", "Start extracting features for dataset: " + dsd.getId());
 
@@ -99,35 +105,36 @@ public class FantailConnector {
 		}
 
 		// first run stream characterizers
-		for(StreamCharacterizer sc : globalMetafeatures.getStreamCharacterizers()) {
+		for (StreamCharacterizer sc : globalMetafeatures.getStreamCharacterizers()) {
 
 			if (qualitiesAvailable.containsAll(Arrays.asList(sc.getIDs())) == false) {
 				Conversion.log("OK", "Extract Features", "Running Stream Characterizers (full data)");
-				//This just precomputes everything, result will be used later depending on the windows size
+				// This just precomputes everything, result will be used later depending on the windows size
 				sc.characterize(dataset);
 			} else {
 				Conversion.log("OK", "Extract Features", "Skipping Stream Characterizers (full data) - already in database");
 			}
 		}
-		
+
 		List<Quality> qualities = new ArrayList<DataQuality.Quality>();
 		if (window_size != null) {
 			Conversion.log("OK", "Extract Features", "Running Batch Characterizers (partial data)");
-			
+
 			for (int i = 0; i < dataset.numInstances(); i += window_size) {
 				if (apiconnector.getVerboselevel() >= Constants.VERBOSE_LEVEL_ARFF) {
-					Conversion.log("OK", "FantailConnector", "Starting window [" + i + "," + (i + window_size) + "> (did = " + dsd.getId() + ",total size = " + dataset.numInstances() + ")");
+					Conversion.log("OK", "FantailConnector",
+							"Starting window [" + i + "," + (i + window_size) + "> (did = " + dsd.getId() + ",total size = " + dataset.numInstances() + ")");
 				}
 				qualities.addAll(datasetCharacteristics(dataset, i, window_size, null));
-				
-				for(StreamCharacterizer sc : globalMetafeatures.getStreamCharacterizers()) {
+
+				for (StreamCharacterizer sc : globalMetafeatures.getStreamCharacterizers()) {
 					// preventing nullpointer exception (if stream characterizer was already run)
 					if (qualitiesAvailable.containsAll(Arrays.asList(sc.getIDs())) == false) {
 						qualities.addAll(hashMaptoList(sc.interval(i), i, window_size));
 					}
 				}
 			}
-			
+
 		} else {
 			Conversion.log("OK", "Extract Features", "Running Batch Characterizers (full data, might take a while)");
 			qualities.addAll(datasetCharacteristics(dataset, null, null, qualitiesAvailable));
@@ -139,14 +146,13 @@ public class FantailConnector {
 			}
 		}
 		Conversion.log("OK", "Extract Features", "Done generating features, start wrapping up");
-		if(qualities.size()>0){
+		if (qualities.size() > 0) {
 			DataQuality dq = new DataQuality(dsd.getId(), qualities.toArray(new Quality[qualities.size()]));
 			String strQualities = xstream.toXML(dq);
-			
+
 			DataQualityUpload dqu = apiconnector.dataQualitiesUpload(Conversion.stringToTempFile(strQualities, "qualities_did_" + dsd.getId(), "xml"));
 			Conversion.log("OK", "Extract Features", "DONE: " + dqu.getDid());
-		}
-		else{
+		} else {
 			Conversion.log("OK", "Extract Features", "DONE: Nothing to upload");
 		}
 	}
@@ -154,7 +160,7 @@ public class FantailConnector {
 	private List<Quality> datasetCharacteristics(Instances dataset, Integer start, Integer interval_size, List<String> qualitiesAvailable) throws Exception {
 		List<Quality> result = new ArrayList<DataQuality.Quality>();
 		Instances intervalData;
-		
+
 		// Be careful changing this!
 		if (interval_size != null) {
 			intervalData = new Instances(dataset, start, Math.min(interval_size, dataset.numInstances() - start));
@@ -164,34 +170,58 @@ public class FantailConnector {
 			intervalData = dataset;
 			// todo: use StringToNominal filter? might be too expensive
 		}
-		
-		for(Characterizer dc : globalMetafeatures.getCharacterizers()) {
-			if (qualitiesAvailable != null && qualitiesAvailable.containsAll(Arrays.asList(dc.getIDs())) == false) { 
-				Conversion.log("OK","Extract Batch Features", dc.getClass().getName() + ": " + Arrays.toString(dc.getIDs()));
-				Map<String,Double> qualities = dc.characterize(intervalData);
+
+		for (Characterizer dc : globalMetafeatures.getCharacterizers()) {
+			if (qualitiesAvailable != null && qualitiesAvailable.containsAll(Arrays.asList(dc.getIDs())) == false) {
+				Conversion.log("OK", "Extract Batch Features", dc.getClass().getName() + ": " + Arrays.toString(dc.getIDs()));
+				Map<String, Double> qualities = dc.characterize(intervalData);
 				result.addAll(hashMaptoList(qualities, start, interval_size));
 			} else {
-				Conversion.log("OK","Extract Batch Features", dc.getClass().getName() + " - already in database");
+				Conversion.log("OK", "Extract Batch Features", dc.getClass().getName() + " - already in database");
 			}
 		}
-        AttributeMetafeatures attributeMetafeatures = new AttributeMetafeatures(dataset.numAttributes());
-        for (AttributeCharacterizer attributeCharacterizer : attributeMetafeatures.getAttributeCharacterizers()) {
-            Map<String,QualityResult> qualities = attributeMetafeatures.characterize(dataset, attributeCharacterizer);
-            result.addAll(attributeMetafeatures.qualityResultToList(qualities, start, interval_size));
-        }
+		AttributeMetafeatures attributeMetafeatures = new AttributeMetafeatures(dataset.numAttributes());
+
+		// parallel computation of attribute meta-features
+		int threads = Runtime.getRuntime().availableProcessors();
+		ExecutorService service = Executors.newFixedThreadPool(threads);
+
+		List<Future<List<Quality>>> futures = new ArrayList<Future<List<Quality>>>();
+		for (final AttributeCharacterizer attributeCharacterizer : attributeMetafeatures.getAttributeCharacterizers()) {
+			Callable<List<Quality>> callable = new Callable<List<Quality>>() {
+				public List<Quality> call() throws Exception {
+					List<Quality> output = new ArrayList<Quality>();
+					Map<String, QualityResult> qualities = attributeMetafeatures.characterize(dataset, attributeCharacterizer);
+					output.addAll(attributeMetafeatures.qualityResultToList(qualities, start, interval_size));
+					return output;
+				}
+			};
+			futures.add(service.submit(callable));
+		}
+
+		service.shutdown();
+
+		for (Future<List<Quality>> future : futures) {
+			result.addAll(future.get());
+		}
+
+		// old for loop
+		// for (AttributeCharacterizer attributeCharacterizer : attributeMetafeatures.getAttributeCharacterizers()) {
+		// Map<String,QualityResult> qualities = attributeMetafeatures.characterize(dataset, attributeCharacterizer);
+		// result.addAll(attributeMetafeatures.qualityResultToList(qualities, start, interval_size));
+		// }
 		return result;
 	}
 
+	public static List<Quality> hashMaptoList(Map<String, Double> map, Integer start, Integer size) {
+		List<Quality> result = new ArrayList<>();
+		for (String quality : map.keySet()) {
+			Integer end = start != null ? start + size : null;
+			result.add(new Quality(quality, map.get(quality) + "", start, end, null));
+		}
+		return result;
+	}
 
-    public static List<Quality> hashMaptoList(Map<String, Double> map, Integer start, Integer size) {
-        List<Quality> result = new ArrayList<>();
-        for(String quality : map.keySet()) {
-            Integer end = start != null ? start + size : null;
-            result.add(new Quality(quality, map.get(quality) + "", start, end, null));
-        }
-        return result;
-    }
-	
 	private static Instances applyFilter(Instances dataset, Filter filter, String options) throws Exception {
 		filter.setOptions(Utils.splitOptions(options));
 		filter.setInputFormat(dataset);
