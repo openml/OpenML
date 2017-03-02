ea2fb8beb041abcc75a05a6dbc4998ff70d39c08
smidj <jakub.smid@merck.com>
2017-03-02 14:59:27 +0100

    Updated queries for getting datasets without computed metafeatures, also it includes datasets even withoyt a single metafeature

--- Java/OpenmlWebapplication/src/org/openml/webapplication/features/AttributeMetafeatures.java
+++ Java/OpenmlWebapplication/src/org/openml/webapplication/features/AttributeMetafeatures.java
@@ -3,10 +3,8 @@ package org.openml.webapplication.features;
 import org.openml.apiconnector.xml.DataQuality;
 import org.openml.webapplication.attributeCharacterization.AttributeCharacterizer;
 import weka.core.Instances;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class AttributeMetafeatures {
     private List<AttributeCharacterizer> attributeCharacterizers = new ArrayList<>();
@@ -38,7 +36,7 @@ public class AttributeMetafeatures {
         return attributeCharacterizers;
     }
 
-    public static int numberOfAttributeMetafeatures(){
-        return AttributeCharacterizer.ids.length;
+    public static List<String> getAttributeMetafeatures(){
+        return Arrays.asList(AttributeCharacterizer.ids);
     }
 }
