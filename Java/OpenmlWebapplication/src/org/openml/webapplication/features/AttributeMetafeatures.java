83fd6f210a88ce9ce65e44742ff907aaac82489d
William Raynaut <william.raynaut@gmail.com>
2017-03-02 13:11:48 +0100

    Fix FantailConnector & AttributeMetafeatures to take account for id & ignored atts

--- Java/OpenmlWebapplication/src/org/openml/webapplication/features/AttributeMetafeatures.java
+++ Java/OpenmlWebapplication/src/org/openml/webapplication/features/AttributeMetafeatures.java
@@ -1,6 +1,7 @@
 package org.openml.webapplication.features;
 
 import org.openml.apiconnector.xml.DataQuality;
+import org.openml.apiconnector.xml.DataSetDescription;
 import org.openml.webapplication.attributeCharacterization.AttributeCharacterizer;
 import weka.core.Instances;
 import java.util.ArrayList;
@@ -9,36 +10,55 @@ import java.util.List;
 import java.util.Map;
 
 public class AttributeMetafeatures {
-    private List<AttributeCharacterizer> attributeCharacterizers = new ArrayList<>();
-    public AttributeMetafeatures(int numAttributes){
-        for (int indexNr =0; indexNr < numAttributes; indexNr++) {
-            AttributeCharacterizer attributeCharacterizer = new AttributeCharacterizer(indexNr);
-            attributeCharacterizers.add(attributeCharacterizer);
-        }
-    }
 
-    public  List<DataQuality.Quality> qualityResultToList(Map<String, QualityResult> map, Integer start, Integer size) {
-        List<DataQuality.Quality> result = new ArrayList<>();
-        for(String quality : map.keySet()) {
-            Integer end = start != null ? start + size : null;
-            QualityResult qualityResult = map.get(quality);
-            result.add(new DataQuality.Quality(quality, qualityResult.value + "", start, end, qualityResult.index));
-        }
-        return result;
-    }
+	private List<AttributeCharacterizer> attributeCharacterizers = new ArrayList<>();
 
-    public Map<String, QualityResult> characterize(Instances instances, AttributeCharacterizer characterizer){
-        Map<String, Double> values =characterizer.characterize(instances);
-        Map <String, QualityResult> result = new HashMap<>();
-        values.forEach((s,v) -> result.put(s, new QualityResult(v, characterizer.getIndex())));
-        return result;
-    }
+	public AttributeMetafeatures(int numAttributes, Instances dataset, DataSetDescription dsd) {
 
-    public List<AttributeCharacterizer> getAttributeCharacterizers(){
-        return attributeCharacterizers;
-    }
+		for (int indexNr = 0; indexNr < numAttributes; indexNr++) {
 
-    public static int numberOfAttributeMetafeatures(){
-        return AttributeCharacterizer.ids.length;
-    }
+			boolean processAtt = true;
+			if (dsd.getRow_id_attribute() != null && dataset.attribute(dsd.getRow_id_attribute()) != null
+					&& dataset.attribute(dsd.getRow_id_attribute()).index() == indexNr) {
+				processAtt = false;
+			}
+			if (dsd.getIgnore_attribute() != null) {
+				for (String att : dsd.getIgnore_attribute()) {
+					if (dataset.attribute(att) != null && dataset.attribute(att).index() == indexNr) {
+						processAtt = false;
+					}
+				}
+			}
+			if (processAtt) {
+				AttributeCharacterizer attributeCharacterizer = new AttributeCharacterizer(indexNr);
+				attributeCharacterizers.add(attributeCharacterizer);
+			}
+
+		}
+	}
+
+	public List<DataQuality.Quality> qualityResultToList(Map<String, QualityResult> map, Integer start, Integer size) {
+		List<DataQuality.Quality> result = new ArrayList<>();
+		for (String quality : map.keySet()) {
+			Integer end = start != null ? start + size : null;
+			QualityResult qualityResult = map.get(quality);
+			result.add(new DataQuality.Quality(quality, qualityResult.value + "", start, end, qualityResult.index));
+		}
+		return result;
+	}
+
+	public Map<String, QualityResult> characterize(Instances instances, AttributeCharacterizer characterizer) {
+		Map<String, Double> values = characterizer.characterize(instances);
+		Map<String, QualityResult> result = new HashMap<>();
+		values.forEach((s, v) -> result.put(s, new QualityResult(v, characterizer.getIndex())));
+		return result;
+	}
+
+	public List<AttributeCharacterizer> getAttributeCharacterizers() {
+		return attributeCharacterizers;
+	}
+
+	public static int numberOfAttributeMetafeatures() {
+		return AttributeCharacterizer.ids.length;
+	}
 }
