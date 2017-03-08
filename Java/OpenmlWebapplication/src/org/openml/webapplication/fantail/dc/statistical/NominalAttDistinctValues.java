4f1e73749fc8bb4609a1a9a04ac9769b0d2465b9
William Raynaut <william.raynaut@gmail.com>
2017-03-08 17:36:47 +0100

    NominalAttDistinctValues cleanup and robustness

--- Java/OpenmlWebapplication/src/org/openml/webapplication/fantail/dc/statistical/NominalAttDistinctValues.java
+++ Java/OpenmlWebapplication/src/org/openml/webapplication/fantail/dc/statistical/NominalAttDistinctValues.java
@@ -1,39 +1,18 @@
-/*
- *  Webapplication - Java library that runs on OpenML servers
- *  Copyright (C) 2014 
- *  @author Jan N. van Rijn (j.n.van.rijn@liacs.leidenuniv.nl)
- *  @author Quan Sun (quan.sun.nz@gmail.com)
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
- */
 package org.openml.webapplication.fantail.dc.statistical;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.openml.webapplication.fantail.dc.Characterizer;
 
 import weka.core.Instances;
-import weka.core.Utils;
 
 public class NominalAttDistinctValues extends Characterizer {
 
-	protected final String[] ids = new String[] {
-			"MaxNominalAttDistinctValues", "MinNominalAttDistinctValues",
-			"MeanNominalAttDistinctValues", "StdvNominalAttDistinctValues" };
+	protected final String[] ids = new String[] { "MaxNominalAttDistinctValues", "MinNominalAttDistinctValues", "MeanNominalAttDistinctValues",
+			"StdvNominalAttDistinctValues" };
 
 	@Override
 	public String[] getIDs() {
@@ -41,50 +20,46 @@ public class NominalAttDistinctValues extends Characterizer {
 	}
 
 	@Override
-	public Map<String, Double> characterize(Instances data) {
-		int attrib_count = data.numAttributes() - 1;
-		int nominal_count = 0;
-
-		for (int i = 0; i < attrib_count; i++) {
-			if (data.attribute(i).isNominal()) {
-				nominal_count++;
-			}
-		}
-
-		if (nominal_count == 0) {
-			Map<String, Double> qualities = new HashMap<String, Double>();
-			qualities.put(ids[0], -1.0);
-			qualities.put(ids[1], -1.0);
-			qualities.put(ids[2], -1.0);
-			qualities.put(ids[3], -1.0);
-			return qualities;
-		}
-
-		ArrayList<Double> distinctValuesCounts = new ArrayList<Double>();
-
-		for (int i = 0; i < attrib_count; i++) {
-			if (data.attribute(i).isNominal()) {
-				distinctValuesCounts.add(1.0 * data.numDistinctValues(i));
-
-			}
-		}
-
-		double[] values = new double[distinctValuesCounts.size()];
-		for (int i = 0; i < distinctValuesCounts.size(); i++) {
-			values[i] = distinctValuesCounts.get(i);
-		}
-
-		double min = values[Utils.minIndex(values)];
-		double max = values[Utils.maxIndex(values)];
-		double mean = Utils.mean(values);
-		double variance = Utils.variance(values);
-		double stdv = Math.sqrt(variance);
-
+	public Map<String, Double> characterize(Instances dataset) {
 		Map<String, Double> qualities = new HashMap<String, Double>();
-		qualities.put(ids[0], max);
-		qualities.put(ids[1], min);
-		qualities.put(ids[2], mean);
-		qualities.put(ids[3], stdv);
+
+		try {
+			DescriptiveStatistics distinctValuesStats = new DescriptiveStatistics();
+			for (int attribute = 0; attribute < dataset.numAttributes(); attribute++) {
+				if (dataset.attribute(attribute).isNominal()) {
+
+					ArrayList<Double> valuesList = new ArrayList<Double>();
+					for (int instances = 0; instances < dataset.numInstances(); instances++) {
+						if (!dataset.get(instances).isMissing(attribute)) {
+							double value = dataset.get(instances).value(attribute);
+							if (!valuesList.contains(value)) {
+								valuesList.add(value);
+							}
+						}
+					}
+
+					distinctValuesStats.addValue(valuesList.size());
+				}
+			}
+
+			qualities.put("MaxNominalAttDistinctValues", distinctValuesStats.getMax());
+			qualities.put("MinNominalAttDistinctValues", distinctValuesStats.getMin());
+			qualities.put("MeanNominalAttDistinctValues", distinctValuesStats.getMean());
+			qualities.put("StdvNominalAttDistinctValues", distinctValuesStats.getStandardDeviation());
+		} catch (Exception e) {
+		}
+
+		// enforce finite double or null for all qualities
+		for (String key : qualities.keySet()) {
+			if (qualities.get(key) != null && !Double.isFinite(qualities.get(key)))
+				qualities.replace(key, null);
+		}
+
+		for (String key : ids) {
+			if (!qualities.containsKey(key))
+				qualities.put(key, null);
+		}
+
 		return qualities;
 	}
 }
