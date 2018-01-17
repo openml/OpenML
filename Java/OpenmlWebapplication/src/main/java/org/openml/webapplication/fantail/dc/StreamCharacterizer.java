45d3c4bc14de07f702d271ea9679415c518cc26c
janvanrijn@gmail.com <janvanrijn@gmail.com>
2018-01-17 14:43:49 +0100

    removed evaluation engine from this repo (moved to openml/EvaluationEngine)

--- Java/OpenmlWebapplication/src/main/java/org/openml/webapplication/fantail/dc/StreamCharacterizer.java
+++
@@ -1,9 +0,0 @@
-package org.openml.webapplication.fantail.dc;
-
-import java.util.Map;
-
-public abstract class StreamCharacterizer extends Characterizer {
-
-	public abstract Map<String, Double> global( );
-	public abstract Map<String, Double> interval( int interval_start );
-}
--- Java/OpenmlWebapplication/src/main/java/org/openml/webapplication/settings/Settings.java
+++
@@ -1,6 +0,0 @@
-package org.openml.webapplication.settings;
-
-public class Settings {
-
-	public static final int EVALUATION_ENGINE_ID = 1;
-}
