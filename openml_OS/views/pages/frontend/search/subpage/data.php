1c1106075947ac2b2669bffdf195e229f75752c9
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-01-23 03:20:56 +0100

    status dropdown

--- openml_OS/views/pages/frontend/search/subpage/data.php
+++ openml_OS/views/pages/frontend/search/subpage/data.php
@@ -23,7 +23,7 @@
        <option value="<?php echo $m;?>" <?php if(array_key_exists('evaluation_measures',$this->filters) and $this->filters['evaluation_measures'] == $m){ echo 'selected';}?>><?php echo str_replace('_', ' ', $m);?></option>
 
          <option value="all" <?php if(array_key_exists('status',$this->filters) and $this->filters['status'] == 'all'){ echo 'selected';}?>>all</option>
-         <option value="active" <?php if(array_key_exists('status',$this->filters) or $this->filters['status'] == 'active'){ echo 'selected';}?>>active</option>
+         <option value="active" <?php if(array_key_exists('status',$this->filters) or $this->filters['status'] == 'active' or $this->filters['status'] == ''){ echo 'selected';}?>>active</option>
          <option value="in_preparation" <?php if(array_key_exists('status',$this->filters) and $this->filters['status'] == 'in_preparation'){ echo 'selected';}?>>in_preparation</option>
          <option value="deactivated" <?php if(array_key_exists('status',$this->filters) and $this->filters['status'] == 'deactivated'){ echo 'selected';}?>>deactivated</option>
     </select>
