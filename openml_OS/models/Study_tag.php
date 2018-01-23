20075028ec18a0459db8d983422c920baf18c396
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-01-23 09:22:00 +0100

    study tagging bugfix

--- openml_OS/models/Study_tag.php
+++ openml_OS/models/Study_tag.php
@@ -6,22 +6,22 @@ class Study_tag extends Database_write {
     $this->table = 'study_tag';
     $this->id_column = array('study_id', 'tag');
   }
-  
+
   // given a tagged entity, return all the studies that need to be updated with it
   function studiesToUpdate($tag, $time, $user_id) {
-    $sql = 
-      'SELECT t.tag FROM study_tag t, study s ' . 
+    $sql =
+      'SELECT t.study_id FROM study_tag t, study s ' .
       'WHERE t.study_id = s.id AND tag = "' . $tag . '" ' .
       'AND (t.write_access = "public" OR s.creator = "' . $user_id . '") ' .
       'AND (t.window_start IS NULL OR t.window_start < "' . $time . '")' .
       'AND (t.window_end IS NULL OR t.window_end > "' . $time . '")';
-    
-    
-    return $this->getColumnFromSql('tag', $sql);
+
+
+    return $this->getColumnFromSql('study_id', $sql);
   }
-  
+
   function getDataIdsFromStudy($study_id) {
-    $sql = 
+    $sql =
       'SELECT d.did, d.name, GROUP_CONCAT(st.tag) AS tags '.
       'FROM study s, study_tag st, dataset_tag t, dataset d '.
       'WHERE d.did = t.id AND t.tag = st.tag AND st.study_id = s.id '.
@@ -29,9 +29,9 @@ class Study_tag extends Database_write {
       'GROUP BY d.did';
     return $this->getColumnFromSql('did', $sql);
   }
-  
+
   function getTaskIdsFromStudy($study_id) {
-    $sql = 
+    $sql =
       'SELECT task.task_id, GROUP_CONCAT(st.tag) AS tags '.
       'FROM study s, study_tag st, task_tag t, task '.
       'WHERE task.task_id = t.id AND t.tag = st.tag AND st.study_id = s.id '.
@@ -39,9 +39,9 @@ class Study_tag extends Database_write {
       'GROUP BY task.task_id';
     return $this->getColumnFromSql('task_id', $sql);
   }
-  
+
   function getFlowIdsFromStudy($study_id) {
-    $sql = 
+    $sql =
       'SELECT i.id, i.name, GROUP_CONCAT(st.tag) AS tags '.
       'FROM study s, study_tag st, implementation_tag t, implementation i '.
       'WHERE i.id = t.id AND t.tag = st.tag AND st.study_id = s.id '.
@@ -49,9 +49,9 @@ class Study_tag extends Database_write {
       'GROUP BY i.id';
     return $this->getColumnFromSql('id', $sql);
   }
-  
+
   function getSetupIdsFromStudy($study_id) {
-    $sql = 
+    $sql =
       'SELECT a.sid, GROUP_CONCAT(st.tag) AS tags '.
       'FROM study s, study_tag st, setup_tag t, algorithm_setup a '.
       'WHERE a.sid = t.id AND t.tag = st.tag AND st.study_id = s.id '.
@@ -59,9 +59,9 @@ class Study_tag extends Database_write {
       'GROUP BY a.sid';
     return $this->getColumnFromSql('sid', $sql);
   }
-  
+
   function getRunIdsFromStudy($study_id) {
-    $sql = 
+    $sql =
       'SELECT r.rid, GROUP_CONCAT(st.tag) AS tags '.
       'FROM study s, study_tag st, run_tag t, run r '.
       'WHERE r.rid = t.id AND t.tag = st.tag AND st.study_id = s.id '.
@@ -69,7 +69,7 @@ class Study_tag extends Database_write {
       'GROUP BY r.rid';
     return $this->getColumnFromSql('rid', $sql);
   }
-  
+
   private function getRestrictions($study_id) {
     return 'AND s.id = ' . $study_id . ' ' .
       'AND (st.window_start IS NULL OR t.date > st.window_start) '.
