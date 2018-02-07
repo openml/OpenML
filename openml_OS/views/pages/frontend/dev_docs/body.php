db0a8584886b2a5584855a5c5da38237d8c48356
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-02-07 12:29:40 +0100

    css tweak

--- openml_OS/views/pages/frontend/dev_docs/body.php
+++ openml_OS/views/pages/frontend/dev_docs/body.php
@@ -1,5 +1,5 @@
 <iframe src="https://openml.github.io/OpenML/"
-        style="height:100vh;width:100%" height="100vh" width="100%" frameborder="0" id="dev_doc_frame"
+        style="height:100vh;width:100%" height="110vh" width="100%" frameborder="0" id="dev_doc_frame"
         allowfullscreen sandbox="allow-scripts allow-same-origin">
   <p> <a href="https://openml.github.io/OpenML/">
     Fallback link for browsers that don't support iframes
