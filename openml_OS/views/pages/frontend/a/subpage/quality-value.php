4eb1a63a1618baaaa2ddbf243b0df8b954c3f70c
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-02-11 01:25:01 +0100

    fixed links for measures

--- openml_OS/views/pages/frontend/a/subpage/quality-value.php
+++ openml_OS/views/pages/frontend/a/subpage/quality-value.php
@@ -6,7 +6,7 @@
 
 	<p>The <a href="<?php echo BASE_URL; ?>a/data-qualities/<?php echo strtolower($this->quality->quality); ?>"><?php echo $this->quality->quality; ?></a> of <a href="<?php echo BASE_URL; ?>d/<?php echo $this->data->did; ?>"><?php echo $this->data->name; ?></a> is <?php echo $this->quality->value; ?></p>
 	<ul class="hotlinks">
-		<li><a href="https://github.com/openml/OpenML/tree/master/Java/OpenmlWebapplication/src/org/openml/webapplication/features"><i class="fa fa-gears"></i> View code</a></li>
+		<li><a href="https://github.com/openml/EvaluationEngine/tree/master/src/main/java/org/openml/webapplication/features"><i class="fa fa-gears"></i> View code</a></li>
 	</ul>
   <?php if( $this->quality->description ): ?>
     <h2>Description</h2>
