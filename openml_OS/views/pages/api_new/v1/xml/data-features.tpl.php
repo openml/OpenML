746003025ffbd6f3559c8ad1487b205362635c6c
janvanrijn <janvanrijn@gmail.com>
2018-08-10 13:43:37 -0400

    fix data features

--- openml_OS/views/pages/api_new/v1/xml/data-features.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/data-features.tpl.php
@@ -4,7 +4,7 @@
     <oml:index><?php echo $feature->index; ?></oml:index>
     <oml:name><?php echo htmlspecialchars($feature->name); ?></oml:name>
     <oml:data_type><?php echo $feature->data_type; ?></oml:data_type>
-    <?php if ($feature->nominal_values != null): ?><oml:nominal_values><?php echo $feature->nominal_values; ?></oml:nominal_values><?php endif; ?>
+    <?php if ($feature->nominal_values != null): ?><oml:nominal_values><?php echo htmlspecialchars($feature->nominal_values); ?></oml:nominal_values><?php endif; ?>
     <oml:is_target><?php echo $feature->is_target; ?></oml:is_target>
     <oml:is_ignore><?php echo $feature->is_ignore; ?></oml:is_ignore>
     <oml:is_row_identifier><?php echo $feature->is_row_identifier; ?></oml:is_row_identifier>
