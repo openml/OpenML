413d6ce834594254b571135a9d8615e038471580
janvanrijn <janvanrijn@gmail.com>
2018-09-20 10:07:19 -0400

    revert error

--- openml_OS/models/api/v1/Api_data.php
+++ openml_OS/models/api/v1/Api_data.php
@@ -731,7 +731,7 @@ class Api_data extends MY_Api_Model {
         }
         
         
-      } elseif ($feature['data_type'] == 'nominal' && !$data_processed_record->error) {
+      } elseif ($feature['data_type'] == 'nominal') {
         // required for nominal values.. missing so throw error
         $this->db->trans_rollback();
         $this->returnError(438, $this->version, $this->openmlGeneralErrorCode, 'feature: ' . $feature['name']);
