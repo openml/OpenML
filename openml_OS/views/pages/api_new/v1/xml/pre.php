efa37bcc81f40fbf595ba761b6cf5a1f68f89b69
janvanrijn <janvanrijn@gmail.com>
2018-09-20 10:49:10 -0400

    improve error messages

--- openml_OS/views/pages/api_new/v1/xml/pre.php
+++ openml_OS/views/pages/api_new/v1/xml/pre.php
@@ -217,8 +217,8 @@ $this->apiErrors[434] = 'Could not find dataset';
 $this->apiErrors[435] = 'Feature upload failed';
 $this->apiErrors[436] = 'Something wrong with XML, check did and evaluation engine id';
 $this->apiErrors[437] = 'Class Distribution not valid json';
-$this->apiErrors[438] = 'Nominal feature is not accompanied with valid json (this can only be tolerated if an error message is also registered)';
-$this->apiErrors[439] = 'Non-Nominal feature obtained json string';
+$this->apiErrors[438] = 'Nominal feature is not accompanied with values';
+$this->apiErrors[439] = 'Non-Nominal feature obtained values';
 
 $this->apiErrors[446] = 'Database insert failed';
 
