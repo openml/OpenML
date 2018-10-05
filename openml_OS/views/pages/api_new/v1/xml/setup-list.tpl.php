f4fe64ecac5f48bbbe57ff9f6460d051ee4cd1cd
janvanrijn <janvanrijn@gmail.com>
2018-10-05 17:26:29 -0400

    updates query and database schema

--- openml_OS/views/pages/api_new/v1/xml/setup-list.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/setup-list.tpl.php
@@ -7,7 +7,7 @@
 	    <oml:parameter>
 		    <oml:id><?php echo htmlspecialchars($p->id); ?></oml:id>
 		    <oml:flow_id><?php echo htmlspecialchars($p->implementation_id); /*important! this is different from $p->flow_id; */?></oml:flow_id>
-		    <oml:full_name><?php echo htmlspecialchars($p->fullName); ?></oml:full_name>
+		    <oml:full_name><?php echo htmlspecialchars($p->flow_fullName) . '_' . htmlspecialchars($p->name); ?></oml:full_name>
 		    <oml:parameter_name><?php echo htmlspecialchars($p->name); ?></oml:parameter_name>
 		    <oml:data_type><?php echo htmlspecialchars($p->dataType); ?></oml:data_type>
 		    <oml:default_value><?php echo htmlspecialchars($p->defaultValue); ?></oml:default_value>
