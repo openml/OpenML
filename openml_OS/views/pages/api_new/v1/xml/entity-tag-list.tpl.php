527db2ce996e823ac3eb73baf91219689aa312c4
janvanrijn <janvanrijn@gmail.com>
2018-10-18 17:08:21 -0400

    same

--- openml_OS/views/pages/api_new/v1/xml/entity-tag-list.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/entity-tag-list.tpl.php
@@ -1,5 +1,5 @@
 <oml:<?php echo $xml_tag_name; ?>_list xmlns:oml="http://openml.org/openml">
   <?php foreach($tags as $tag): ?>
   <oml:tag><?php echo $tag; ?></oml:tag>
-  <?php endforeach; endif; ?>
+  <?php endforeach; ?>
 </oml:<?php echo $xml_tag_name; ?>_list>
