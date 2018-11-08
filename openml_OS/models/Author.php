bf622331acf5e8a088c7e05fb30e0124a362b1cb
janvanrijn <janvanrijn@gmail.com>
2018-11-07 22:53:15 -0500

    updated author

--- openml_OS/models/Author.php
+++ openml_OS/models/Author.php
@@ -1,10 +1,12 @@
 <?php
+// JvR: I want to deprecate this class.
 class Author extends MY_Community_Model {
 	
 	function __construct()
     {
 		parent::__construct();
 		$this->table = 'users';
+		$this->id_column = 'id';
 		$this->deleted_activated = 'id IS NOT NULL ';
     }
     
