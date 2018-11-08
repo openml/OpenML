b9bca4d636acafc0b519bc27c6dff58b1e6bee20
janvanrijn <janvanrijn@gmail.com>
2018-11-07 23:04:08 -0500

    small fix

--- openml_OS/models/Users.php
+++ openml_OS/models/Users.php
@@ -16,7 +16,7 @@ class Users extends MY_Community_Model {
     $additional_data = array();
     resize_image_squared($tmp_file_location, $this->config->item('max_avatar_size'));
     $to_directory = 'userdata_structured/' . $user_id;
-    $subdirectory = floor($id / $this->content_folder_modulo) * $this->content_folder_modulo;
+    $subdirectory = floor($user_id / $this->content_folder_modulo) * $this->content_folder_modulo;
     $file_id = $this->File->register_uploaded_file($_FILES['image'], $to_directory, $user_id, 'userimage');
     if($file_id) {
       $additional_data['image'] = $this->config->item('data_controller') . '/view/' . $file_id . '/' . $_FILES['image']['name'];
