4d826e6dec877920146697235ead96bcd75cdbd3
janvanrijn <janvanrijn@gmail.com>
2018-11-07 22:34:07 -0500

    user image upload centralization

--- openml_OS/models/Users.php
+++ openml_OS/models/Users.php
@@ -5,7 +5,25 @@ class Users extends MY_Community_Model {
     parent::__construct();
     $this->table = 'users';
     $this->id_column = 'id';
-    $this->deleted_activated = 'id IS NOT NULL ';
+    
+    $this->load->model('File');
+    $this->load->helper('image');
   }
+  
+  function update_image($user_id, $tmp_file_location) {
+    $additional_data = array();
+    resize_image_squared($tmp_file_location, $this->config->item('max_avatar_size'));
+    $to_directory = 'userdata_structured/' . $user_id;
+    $subdirectory = floor($id / $this->content_folder_modulo) * $this->content_folder_modulo;
+    $file_id = $this->File->register_uploaded_file($_FILES['image'], $to_directory, $userId, 'userimage');
+    if($file_id) {
+      $additional_data['image'] = $this->config->item('data_controller') . '/view/' . $file_id . '/' . $_FILES['image']['name'];
+	    $update = $this->ion_auth->update($user_id, $additional_data['image']);
+	    return $update;
+    } else {
+      return false;
+    }
+  }
+  
 }
 ?>
