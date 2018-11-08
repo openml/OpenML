611bcd856e970aa1331652fb1e9ff65af8298a5b
janvanrijn <janvanrijn@gmail.com>
2018-11-07 23:03:37 -0500

    small fixes

--- openml_OS/models/Users.php
+++ openml_OS/models/Users.php
@@ -8,6 +8,8 @@ class Users extends MY_Community_Model {
     
     $this->load->model('File');
     $this->load->helper('file');
+    
+    $this->content_folder_modulo = 10000;
   }
   
   function update_image($user_id, $tmp_file_location) {
@@ -15,7 +17,7 @@ class Users extends MY_Community_Model {
     resize_image_squared($tmp_file_location, $this->config->item('max_avatar_size'));
     $to_directory = 'userdata_structured/' . $user_id;
     $subdirectory = floor($id / $this->content_folder_modulo) * $this->content_folder_modulo;
-    $file_id = $this->File->register_uploaded_file($_FILES['image'], $to_directory, $userId, 'userimage');
+    $file_id = $this->File->register_uploaded_file($_FILES['image'], $to_directory, $user_id, 'userimage');
     if($file_id) {
       $additional_data['image'] = $this->config->item('data_controller') . '/view/' . $file_id . '/' . $_FILES['image']['name'];
 	    $update = $this->ion_auth->update($user_id, $additional_data['image']);
