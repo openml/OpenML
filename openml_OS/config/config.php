269277a228cf87209e779efdec86eb093f37f463
janvanrijn <janvanrijn@gmail.com>
2018-11-30 15:03:01 -0500

    removed base url from config

--- openml_OS/config/config.php
+++ openml_OS/config/config.php
@@ -391,7 +391,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= BASE_URL;
+$config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= TRUE;
 $config['cookie_httponly'] 	= FALSE;
