6ad4a04af2deaf6d110f56920404ed37ad95baa1
janvanrijn <janvanrijn@gmail.com>
2018-11-30 14:25:38 -0500

    try other code

--- openml_OS/views/frontend_main.php
+++ openml_OS/views/frontend_main.php
@@ -334,23 +334,23 @@ if (session_status() === PHP_SESSION_NONE){session_start();}
               'js/libs/jquery.form.js',
               'js/libs/perfect-scrollbar.min.js',
               'js/openml.js',
-              
+              <?php if( isset( $this->load_javascript ) ): foreach( $this->load_javascript as $j ):
+                echo "'".$j."',"; endforeach; endif; ?>
+              <?php if( isset( $this->id) || $ch == 'new'){
+                echo "'frontend/js/".$req."',";
+              } elseif( $ch == 'search'){
+                echo "'frontend/js/".$reqall."',";
+              } elseif( $ch == 'api'){
+                echo "'frontend/js/api_docs',";
+              } elseif( $ch != 'backend') {
+                if ($ch == 'u' && $id) {
+                    echo "'frontend/js/" . $ch . "/" . $id . "',";
+                } else {
+                    echo "'frontend/js/".$ch."',";
+                }
+              }?>
+              'js/openmlafter.js'
             ])
-          //download js that does not affect DOM and can be loaded after page is rendered
-          if (window.addEventListener)
-          window.addEventListener("load", downloadJSAtOnload, false);
-          else if (window.attachEvent)
-          window.attachEvent("onload", downloadJSAtOnload);
-          else window.onload = downloadJSAtOnload;
-        </script>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-          ga('require', 'linkid', 'linkid.js');
-          ga('create', 'UA-40902346-1', 'openml.org');
-          ga('send', 'pageview');
         </script>
 
     </body>
