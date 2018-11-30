09f8c172b3f961c4f30c01bab5ad6bebdd65c170
janvanrijn <janvanrijn@gmail.com>
2018-11-30 14:35:04 -0500

    updated bodys

--- openml_OS/views/auth/reset_password.php
+++ openml_OS/views/auth/reset_password.php
@@ -1,22 +1,26 @@
-<h1><?php echo lang('reset_password_heading');?></h1>
+<div class="col-xs-12 col-md-10 col-md-offset-1" id="mainpanel">
+  <div class="tab-content">
+    <h1><?php echo lang('reset_password_heading');?></h1>
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <div id="infoMessage"><?php echo $message;?></div>
 
-<?php echo form_open('auth/reset_password/' . $code);?>
+    <?php echo form_open('auth/reset_password/' . $code);?>
 
-	<p>
-		<label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
-		<?php echo form_input($new_password);?>
-	</p>
+	    <p>
+		    <label for="new_password"><?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?></label> <br />
+		    <?php echo form_input($new_password);?>
+	    </p>
 
-	<p>
-		<?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-		<?php echo form_input($new_password_confirm);?>
-	</p>
+	    <p>
+		    <?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?> <br />
+		    <?php echo form_input($new_password_confirm);?>
+	    </p>
 
-	<?php echo form_input($user_id);?>
-	<?php echo form_hidden($csrf); ?>
+	    <?php echo form_input($user_id);?>
+	    <?php echo form_hidden($csrf); ?>
 
-	<p><?php echo form_submit('submit', lang('reset_password_submit_btn'));?></p>
+	    <p><?php echo form_submit('submit', lang('reset_password_submit_btn'));?></p>
 
-<?php echo form_close();?>
+    <?php echo form_close();?>
+  </div>
+</div>
