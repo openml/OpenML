398c30ae8be261d64762805cecf3aa56d95764ad
janvanrijn <janvanrijn@gmail.com>
2018-11-30 15:11:09 -0500

    removed cookie secure thingy

--- openml_OS/config/config.php
+++ openml_OS/config/config.php
@@ -393,7 +393,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= TRUE;
+$config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
 
 /*
