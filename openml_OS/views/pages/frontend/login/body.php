7cfbdfe893f1d9dc19e8caf428cbe5173f6996c8
janvanrijn <janvanrijn@gmail.com>
2018-11-30 11:37:12 -0500

    login form update

--- openml_OS/views/pages/frontend/login/body.php
+++ openml_OS/views/pages/frontend/login/body.php
@@ -6,13 +6,12 @@
     <h2 class="modal-title">Sign in</h2>
   </div>
   <div class="modal-body">
-
-    <form action="login" method="post" accept-charset="utf-8">
+    <?php echo form_open("auth/login");?>
         <fieldset>
           <div class="inputs">
             <input type="hidden" name="location" value="<?php echo str_replace('OpenML/','',$_SERVER['REQUEST_URI']); ?>"/>
-            <input class='form-control floating-label' type="text" name="identity" value="" placeholder="Email" id="identity"  />
-            <input class='form-control floating-label' type="password" name="password" value="" placeholder="Password" id="password"  />
+            <?php echo form_input($identity);?>
+            <?php echo form_input($password);?>
           </div>
           <div class="form-group">
             <input class='btn btn-primary' type="submit" name="submitlogin" value="Sign in"  />
@@ -20,7 +19,7 @@
             <a href="auth/forgot_password" class="btn pull-right">Forgot password</a>
           </div>
         </fieldset>
-    </form>
+    <?php echo form_close();?>
   </div>
  </div>
 </div>
