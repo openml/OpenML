2578e6ccbf62eeacd4c81cc5d138fedb3bd3460c
janvanrijn <janvanrijn@gmail.com>
2018-11-30 12:14:00 -0500

    added config

--- openml_OS/config/config.php
+++ openml_OS/config/config.php
@@ -438,7 +438,7 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
