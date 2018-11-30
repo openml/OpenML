7af52a53d82fe982b11fe675de25b3c65fca27db
janvanrijn <janvanrijn@gmail.com>
2018-11-30 12:19:40 -0500

    config update

--- openml_OS/config/config.php
+++ openml_OS/config/config.php
@@ -391,9 +391,9 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
+$config['cookie_domain']	= BASE_URL;
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
+$config['cookie_secure']	= TRUE;
 $config['cookie_httponly'] 	= FALSE;
 
 /*
