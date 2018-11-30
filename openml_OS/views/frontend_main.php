1d18f6c5f8c1132cf4d0a8811df45c0a7ea18b63
janvanrijn <janvanrijn@gmail.com>
2018-11-30 13:42:42 -0500

    removed more

--- openml_OS/views/frontend_main.php
+++ openml_OS/views/frontend_main.php
@@ -20,8 +20,52 @@ if (session_status() === PHP_SESSION_NONE){session_start();}
 
     <body>
   
+        <div id="sectiontitle"><?php echo $section;?></div>
+        <div class="navbar navbar-static-top navbar-fixed-top navbar-material-<?php echo $materialcolor;?>" id="openmlheader" style="margin-bottom: 0px;">
+            <div class="navbar-inner">
+              <div class="col-xs-6 col-sm-3 col-md-3">
+              <div class="nav pull-left">
+                <a class="navbar-brand menubutton"><i class="fa fa-bars fa-lg"></i></a>
+              </div>
+              <a class="navbar-brand" id="section-brand" href="home">OpenML</a>
+              </div>
+
+
+                    <!--/.nav-collapse -->
+            </div>
+        </div>
+
+
+
+
+          <div class="searchbarcontainer">
+          <div class="searchbar" id="mainmenu" <?php if($section == "OpenML" or $section == "Guide" or $ch == "new" or $ch == "backend"){echo 'style="display:none"';}?>>
+            <div class="sidebar-overlay">
+            <div class="nav pull-left">
+              <a class="navbar-brand menubutton"><i class="fa fa-bars fa-lg"></i></a>
+            </div>
+            <a class="navbar-brand" id="section-brand" href="<?php echo $href; ?>">OpenML</a>
+            </div>
+            <ul class="sidenav nav topchapter" id="topaccordeon">
+              <li class="panel mainchapter">
+                <a data-toggle="collapse" data-parent="#topaccordeon" data-target="#mainlist"> <b>Explore</b></a>
+                <ul class="sidenav nav collapse in" id="mainlist">
+                  
+                      
+                </ul>
+              </li>
+                <li class="menu-cite <?php echo ($section == 'Guide' ?  'topactive' : '');?>"><a href="https://docs.openml.org" class="icongreen"><i class="fa fa-fw fa-lg fa-leanpub"></i> <b>Help</b></a></li>
+                <li class="menu-cite"><a href="https://medium.com/open-machine-learning" class="iconyellow" target="_blank"><i class="fa fa-fw fa-lg fa-rss-square"></i> <b>Blog</b></a></li>
+                <li class="menu-cite <?php echo ($section == 'Contact' ?  'topactive' : '');?>"><a href="contact" class="iconblue"><i class="fa fa-fw fa-lg fa-bullhorn"></i> <b>Contact</b></a></li>
+                <li class="menu-cite <?php echo ($section == 'Citing' ?  'topactive' : '');?>"><a href="cite" class="iconred"><i class="fa fa-fw fa-lg fa-heart"></i> <b>Please cite us</b></a></li>
+            </ul>
+          </div>
+        </div>
 
         <?php echo $body; ?>
+
+
+        </div>
 
 
     </body>
