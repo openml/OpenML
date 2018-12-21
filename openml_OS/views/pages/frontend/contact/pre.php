afae6283c20d7c7eb7bdc2677adaccff90c3faec
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-12-21 11:43:47 +0100

    bugfix

--- openml_OS/views/pages/frontend/contact/pre.php
+++ openml_OS/views/pages/frontend/contact/pre.php
@@ -7,7 +7,7 @@ $this->team = array();
 $this->coreteam = array();
 $core_ids = array(1,2,27,86,348,970,1140);
 foreach( $allteam as $t ) {
-  if( in_array($t->id, $core_ids){
+  if( in_array($t->id, $core_ids) ) {
     array_push($this->coreteam, $t);
   } else {
     array_push($this->team, $t);
