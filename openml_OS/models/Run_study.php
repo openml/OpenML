4c311c1746e1d8099d18875673e12ff8a3b60d4c
janvanrijn <janvanrijn@gmail.com>
2019-02-16 17:16:06 +0100

    bug fix

--- openml_OS/models/Run_study.php
+++ openml_OS/models/Run_study.php
@@ -10,10 +10,10 @@ class Run_study extends MY_Tag_Model {
     $select = 't.task_id, s.sid AS setup_id, i.implementation_id AS flow_id, value as data_id';
     $from = 'run_study rs, algorithm_setup s, run r';
     $conditions = array(
-      '`rs`.`study_id`' => $study_id,
-      '`rs`.`run_id`' => '`r`.`rid`',
-      '`r`.`setup`' => '`s`.`sid`',
-      '`t`.`task_id`' => '`t`.`task_id`',
+      '`rs`.`study_id` = ' . $study_id,
+      '`rs`.`run_id` = `r`.`rid`',
+      '`r`.`setup` = `s`.`sid`',
+      '`t`.`task_id` = `t`.`task_id`',
       
     );
     $this->db->select($select)->from($from)->join('task_inputs t', 'r.task_id = t.task_id AND t.input = "source_data"', 'left')->where($conditions);
