69ac7ffd6c20cdb2fa46fc18ab7e89b626175e24
janvanrijn <janvanrijn@gmail.com>
2019-02-16 17:15:04 +0100

    query update

--- openml_OS/models/Run_study.php
+++ openml_OS/models/Run_study.php
@@ -7,13 +7,13 @@ class Run_study extends MY_Tag_Model {
   }
   
   function get_entities($study_id) {
-    $select = 't.task_id, s.setup_id, i.implementation_id AS flow_id, value as data_id';
+    $select = 't.task_id, s.sid AS setup_id, i.implementation_id AS flow_id, value as data_id';
     $from = 'run_study rs, algorithm_setup s, run r';
     $conditions = array(
-      'rs.study_id' => $study_id,
-      'rs.run_id' => 'r.rid',
-      'r.setup' => 's.sid',
-      't.task_id' => 't.task_id',
+      '`rs`.`study_id`' => $study_id,
+      '`rs`.`run_id`' => '`r`.`rid`',
+      '`r`.`setup`' => '`s`.`sid`',
+      '`t`.`task_id`' => '`t`.`task_id`',
       
     );
     $this->db->select($select)->from($from)->join('task_inputs t', 'r.task_id = t.task_id AND t.input = "source_data"', 'left')->where($conditions);
