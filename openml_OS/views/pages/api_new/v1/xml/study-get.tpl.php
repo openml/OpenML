8ef35bf44fd6a78c29688e84221e08eee05a22b3
janvanrijn <janvanrijn@gmail.com>
2019-02-20 16:48:03 +0100

    update study

--- openml_OS/views/pages/api_new/v1/xml/study-get.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/study-get.tpl.php
@@ -1,7 +1,7 @@
 <oml:study xmlns:oml="http://openml.org/openml">
   <oml:id><?php echo $study->id; ?></oml:id>
   <?php if ($study->alias != null): ?><oml:alias><?php echo $study->alias; ?></oml:alias><?php endif; ?>
-  <oml:main_entity_type><?php echo $study->main_knowledge_type; ?></oml:main_entity_type>
+  <oml:main_entity_type><?php echo $study->main_entity_type; ?></oml:main_entity_type>
   <?php if ($study->benchmark_suite != null): ?><oml:benchmark_suite><?php echo $study->benchmark_suite; ?></oml:benchmark_suite><?php endif; ?>
   <oml:name><?php echo $study->name; ?></oml:name>
   <oml:description><?php echo $study->description; ?></oml:description>
