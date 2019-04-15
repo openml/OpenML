f99f41a4f0d63ff34f5dd50fef3f0f72bde91fc1
janvanrijn <janvanrijn@gmail.com>
2019-04-15 18:36:37 +0200

    added more template stuff

--- openml_OS/views/auth/email/activate.tpl.php
+++ openml_OS/views/auth/email/activate.tpl.php
@@ -4,5 +4,8 @@
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
 
     <p>(If the above link does not display correctly, please visit: <?php site_url('auth/activate/'. $id .'/'. $activation);?>)</p>
+    
+    <p>Kind regards,</p>
+    <p>The OpenML Team</p>
 </body>
 </html>
