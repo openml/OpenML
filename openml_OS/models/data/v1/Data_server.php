8f4aa8d0cd18af150b56242879380a52acfb647d
janvanrijn <janvanrijn@gmail.com>
2019-04-17 15:33:48 +0200

    small fixes

--- openml_OS/models/data/v1/Data_server.php
+++ openml_OS/models/data/v1/Data_server.php
@@ -33,8 +33,8 @@ class Data_server extends CI_Model {
       return;
     }
     
-    if (filesize($file->filepath) != $file->filesize) {
-      $this->_email_filesize_error();
+    if (filesize(DATA_PATH . $file->filepath) != $file->filesize) {
+      $this->_email_filesize_error($file);
       $this->_error404();
       return;
     }
@@ -65,8 +65,8 @@ class Data_server extends CI_Model {
       return;
     }
     
-    if (filesize($file->filepath) != $file->filesize) {
-      $this->_email_filesize_error();
+    if (filesize(DATA_PATH . $file->filepath) != $file->filesize) {
+      $this->_email_filesize_error($file);
       $this->_error404();
       return;
     }
@@ -202,7 +202,7 @@ class Data_server extends CI_Model {
   private function _email_filesize_error($file) {
     $to = EMAIL_API_LOG;
     $subject = 'OpenML File Error. Id: ' . $file->id;
-    $content = 'File record with id' . $file->id . ' (' . $file->type . ') states ' . $file->filesize . ' but PHP filesize states: ' . filesize($file->filepath);
+    $content = 'File record with id' . $file->id . ' (' . $file->type . ') states ' . $file->filesize . ' but PHP filesize states: ' . filesize(DATA_PATH . $file->filepath);
     sendEmail($to, $subject, $content,'text');
   }
 }
