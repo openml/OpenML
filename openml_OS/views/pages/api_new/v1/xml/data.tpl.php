031e52ba688b960d3d065e5bf79dcb27eb7bc32b
janvanrijn <janvanrijn@gmail.com>
2019-05-29 18:13:14 +0100

    added uploader filter

--- openml_OS/views/pages/api_new/v1/xml/data.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/data.tpl.php
@@ -4,6 +4,7 @@
     <oml:did><?php echo $data->did; ?></oml:did>
     <oml:name><?php echo $data->name; ?></oml:name>
     <oml:version><?php echo $data->version; ?></oml:version>
+    <oml:uploader><?php echo $data->uploader; ?></oml:uploader>
     <oml:status><?php echo $data->status; ?></oml:status>
     <oml:format><?php echo $data->format; ?></oml:format>
     <?php if ($data->file_id != null): /* note that this is an optional field! */?>
