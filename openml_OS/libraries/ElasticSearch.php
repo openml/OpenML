7ac00ba52a4f0693ecf806146b407d2a7dd41d4a
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-10-31 01:21:06 +0100

    ES robustness bandaid

--- openml_OS/libraries/ElasticSearch.php
+++ openml_OS/libraries/ElasticSearch.php
@@ -1064,10 +1064,12 @@ class ElasticSearch {
 
     private function fetch_runfiles($min, $max) {
         $index = array();
-        foreach ($this->db->query('SELECT source, field, name, format, file_id from runfile where source >= ' . $min . ' and source < ' . $max) as $r) {
-            $index[$r->source][$r->field]['url'] = BASE_URL . 'data/download/' . $r->file_id . '/' . $r->name;
-            $index[$r->source][$r->field]['format'] = $r->format;
-        }
+        $runfiles = $this->db->query('SELECT source, field, name, format, file_id from runfile where source >= ' . $min . ' and source < ' . $max);
+        if ($runfiles)
+          foreach ($runfiles as $r) {
+              $index[$r->source][$r->field]['url'] = BASE_URL . 'data/download/' . $r->file_id . '/' . $r->name;
+              $index[$r->source][$r->field]['format'] = $r->format;
+          }
         return $index;
     }
 
@@ -1439,16 +1441,16 @@ class ElasticSearch {
         );
 
         $inputs = $this->db->query('SELECT name, type, description, io, requirement FROM task_type_inout where ttid=' . $d->ttid);
-
-        foreach ($inputs as $i) {
-            $new_data['input'][] = array(
-                'name' => $i->name,
-                'type' => $i->type,
-                'description' => $i->description,
-                'io' => $i->io,
-                'requirement' => $i->requirement
-            );
-        }
+        if ($inputs)
+          foreach ($inputs as $i) {
+              $new_data['input'][] = array(
+                  'name' => $i->name,
+                  'type' => $i->type,
+                  'description' => $i->description,
+                  'io' => $i->io,
+                  'requirement' => $i->requirement
+              );
+          }
         return $new_data;
     }
 
@@ -1767,15 +1769,16 @@ class ElasticSearch {
         if ($id and ! $datasets)
             return 'Error: data set ' . $id . ' is unknown';
 
-        foreach ($datasets as $d) {
-            $params['body'][] = array(
-                'index' => array(
-                    '_id' => $d->did
-                )
-            );
+        if ($datasets)
+          foreach ($datasets as $d) {
+              $params['body'][] = array(
+                  'index' => array(
+                      '_id' => $d->did
+                  )
+              );
 
-            $params['body'][] = $this->build_data($d);
-        }
+              $params['body'][] = $this->build_data($d);
+          }
 
         $responses = $this->client->bulk($params);
 
