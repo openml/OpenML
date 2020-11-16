c66e2ec83151cefa5d74f62832337e13ad6e9a43
root <root@capa.win.tue.nl>
2020-11-17 00:23:19 +0100

    script update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Get info
+read -p 'MYSQL Username: ' uservar
+read -sp 'MYSQL Password: ' passvar
+
 # Variables
 OPENML_DIR=$PWD                   # Default is current directory
 OPENML_DATA="$PWD/data"           # Set to data directory
 OPENML_LOG="$OPENML_DATA/log"
-MYSQL_USER="youruser"             # MySQL user name and password
-MYSQL_PASS="yourpassword"
+MYSQL_USER=$uservar             # MySQL user name and password
+MYSQL_PASS=$passvar
 
 echo "Creating directories... "
-mkdir $OPENML_DATA
+#mkdir $OPENML_DATA
 mkdir $OPENML_LOG
 sudo chown apache:apache $OPENML_LOG
 sudo chown apache:apache "$OPENML_LOG/*"
@@ -18,8 +22,8 @@ cd $OPENML_DIR
 
 # reinitiate the database
 echo "Building databases... "
-mysql -u $MYSQL_USER -p $MYSQL_PASS < downloads/openml_expdb.sql
-mysql -u $MYSQL_USER -p $MYSQL_PASS < downloads/openml.sql
+mysql -u $MYSQL_USER -p$MYSQL_PASS < downloads/openml.sql
+mysql -u $MYSQL_USER -p$MYSQL_PASS < downloads/openml_expdb.sql
 
 # fill important columns
 sudo php index.php cron install_database
