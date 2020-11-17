c9f4bc297ea332c5c3cfead81688e38aea87e60e
root <root@capa.win.tue.nl>
2020-11-17 01:26:16 +0100

    simplify reset script

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,6 +23,7 @@ cd $OPENML_DIR
 # reinitiate the database
 echo "Building databases... "
 mysql -u $MYSQL_USER -p$MYSQL_PASS < downloads/openml.sql
+mysql -u $MYSQL_USER -p$MYSQL_PASS openml < /home/jvanscho/users-secret.sql
 mysql -u $MYSQL_USER -p$MYSQL_PASS < downloads/openml_expdb.sql
 
 # fill important columns
@@ -30,115 +31,4 @@ sudo php index.php cron install_database
 
 # create ES search index
 echo "Building search indices... "
-curl -XPUT 'localhost:9200/data' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/run' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/flow' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/task' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/user' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/measure' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/task_type' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/study' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/like' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/download' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-curl -XPUT 'localhost:9200/downvote' -H 'Content-Type: application/json' -d'
-{
-    "settings" : {
-        "index" : {
-            "number_of_shards" : 1,
-            "number_of_replicas" : 0
-        }
-    }
-}
-'
-
 sudo php index.php cron build_es_indices
