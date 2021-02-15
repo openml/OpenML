8764d3356834625b5ed8f9671914f01d53aa7788
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2021-02-15 16:32:48 +0100

    return first and last name

--- openml_OS/views/pages/api_new/v1/xml/user-name.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/user-name.tpl.php
@@ -1,8 +1,8 @@
 <oml:users xmlns:oml="http://openml.org/openml">
   <?php foreach( $users as $u ): ?>
   <oml:user>
-    <oml:id><?php echo $u->id ?></oml:id>
-    <oml:username><?php echo $u->username ?></oml:username>
+    <oml:id><?php echo $u->id; ?></oml:id>
+    <oml:username><?php echo implode(' ', array_filter([$u->first_name, $u->last_name])); ?></oml:username>
   </oml:user>
   <?php endforeach; ?>
 </oml:users>
