ef077149ea4c21219b05da9ac0f1cc6fb1b6214f
sahithyaravi1493 <sahithyaravi1493@gmail.com>
2021-02-23 12:26:01 +0100

    more debug

--- openml_OS/models/api/v1/Api_data.php
+++ openml_OS/models/api/v1/Api_data.php
@@ -1191,7 +1191,8 @@ class Api_data extends MY_Api_Model {
     if ($datasetpqProvided) {
       echo "entered Parquet";
       $pq_filepath = $_FILES['dataset']['tmp_name'];
-      exec('source /etc/profile');
+      $message = exec('source /etc/profile');
+      print_r($message);
       $message = exec("python3 minio_upload.py ".$id.' '.$pq_filepath. "2>&1");
       print_r($message);
 
