fcbd13628c2b09ec6fd8275d8531d20b34f67000
janvanrijn <janvanrijn@gmail.com>
2022-11-18 11:34:12 +0100

    fixes problem

--- openml_OS/models/Data_feature_description.php
+++ openml_OS/models/Data_feature_description.php
@@ -1,5 +1,5 @@
 <?php
-class Data_feature_value extends MY_Database_Write_Model {
+class Data_feature_description extends MY_Database_Write_Model {
   
   function __construct() {
     parent::__construct();
