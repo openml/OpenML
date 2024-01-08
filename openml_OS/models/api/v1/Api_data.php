4d60b8f540a18f457ecc3249cfaf3f7fb2b2082e
root <root@openml.test.tue.nl>
2024-01-08 10:15:59 +0000

    Update to dataset description in test server, remove old minio_url field

--- openml_OS/models/api/v1/Api_data.php
+++ openml_OS/models/api/v1/Api_data.php
@@ -769,10 +769,10 @@ class Api_data extends MY_Api_Model {
     if ($data_status != false) {
       $dataset->status = $data_status->status;
     }
-    if ($dataset->format != 'Sparse_ARFF') {
-      $dataset->parquet_url = 'http://openml1.win.tue.nl/dataset' . $data_id . '/dataset_' . $data_id . '.pq';      
-      $dataset->minio_url = 'http://openml1.win.tue.nl/dataset' . $data_id . '/dataset_' . $data_id . '.pq';
-    }
+    // if ($dataset->format != 'Sparse_ARFF') {
+    //  $dataset->parquet_url = 'http://openml1.win.tue.nl/dataset' . $data_id . '/dataset_' . $data_id . '.pq';      
+    //  $dataset->minio_url = 'http://openml1.win.tue.nl/dataset' . $data_id . '/dataset_' . $data_id . '.pq';
+    // }
       $this->xmlContents( 'data-get', $this->version, $dataset );
   }
 
