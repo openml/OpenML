2962a4c02545d15689bba0e039d0b28cade167c4
janvanrijn <janvanrijn@gmail.com>
2024-01-09 16:33:09 +0100

    api change

--- openml_OS/models/api/v1/Api_data.php
+++ openml_OS/models/api/v1/Api_data.php
@@ -210,14 +210,14 @@ class Api_data extends MY_Api_Model {
         return false;
       }
     } else {
-      $descriptions = $this->Data_feature_description->getColumnWhere('value', 'did = ' . $data_id . ' AND `description_type` = "' . $description_type . '" AND `value` = "' . $description . '"');
-      if ($descriptions == false) {
+      $description_record = $this->Data_feature_description->getWhereSingle('did = ' . $data_id . ' AND index = "' . $index . '" AND `description_type` = "' . $description_type . '" AND `value` = "' . $description . '"');
+      if ($description_record == false) {
         $this->returnError(1103, $this->version);
         return false;
       }
       // todo discuss policy: who is allowed to remove ontology from a feature?
       $is_admin = $this->ion_auth->is_admin($this->user_id);
-      if ($tag_record->uploader != $this->user_id && $is_admin == false) {
+      if ($description_record->uploader != $this->user_id && $is_admin == false) {
         $this->returnError(1104, $this->version);
         return false;
       }
