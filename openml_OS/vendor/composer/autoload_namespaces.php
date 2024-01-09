e6c29828fc7a103ece858fff8e680992711e591b
Jos van der Velde <j.d.v.d.velde@tue.nl>
2024-01-09 11:44:41 +0100

    Updated the dependencies (vendor) and removed it from gitignore

--- openml_OS/vendor/composer/autoload_namespaces.php
+++ openml_OS/vendor/composer/autoload_namespaces.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'org\\bovigo\\vfs' => array($vendorDir . '/mikey179/vfsstream/src/main/php'),
+    'org\\bovigo\\vfs\\' => array($vendorDir . '/mikey179/vfsstream/src/main/php'),
 );
