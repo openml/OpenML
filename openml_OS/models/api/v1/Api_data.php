b6d30166d04b95880499c5ea1e09386f7e546890
janvanrijn <janvanrijn@gmail.com>
2024-01-10 09:44:25 +0100

    adds feature index to query

--- openml_OS/models/api/v1/Api_data.php
+++ openml_OS/models/api/v1/Api_data.php
@@ -188,7 +188,7 @@ class Api_data extends MY_Api_Model {
       return false;
     }
     if ($do_add) {
-      $descriptions = $this->Data_feature_description->getColumnWhere('value', '`did` = ' . $data_id . ' AND `description_type` = "' . $description_type . '"');
+      $descriptions = $this->Data_feature_description->getColumnWhere('value', '`did` = "' . $data_id . '" AND `index` = "'. $feature_idx . '" AND `description_type` = "' . $description_type . '"');
       if($descriptions != false && in_array($description, $descriptions)) {
         $this->returnError(1101, $this->version, 450, 'id=' . $data_id . '; description=' . $description);
         return false;
