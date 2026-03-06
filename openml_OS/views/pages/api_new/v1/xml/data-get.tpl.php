0deebbf62983b4d83afe7000d4a3cbb8eee6395c
Ritu RAj Arvind <ritu.p25@medhaviskillsuniversity.edu.in>
2026-03-07 05:20:07 +0530

    Add uploader field to dataset API response

--- openml_OS/views/pages/api_new/v1/xml/data-get.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/data-get.tpl.php
@@ -9,6 +9,9 @@
   <?php if( is_array( $contributor ) ) foreach( $contributor as $c ): ?><oml:contributor><?php echo htmlspecialchars($c); ?></oml:contributor><?php endforeach; ?>
   <?php if ($collection_date != null): ?> <oml:collection_date><?php echo htmlspecialchars($collection_date); ?></oml:collection_date><?php endif; ?>
   <oml:upload_date><?php echo htmlspecialchars(dateXml($upload_date)); ?></oml:upload_date>
+  <?php if (is_array($creator)) foreach ($creator as $c) : ?>
+  <oml:uploader><?php echo htmlspecialchars($c); ?></oml:uploader>
+  <?php endforeach; ?>
   <?php if ($language != null): ?><oml:language><?php echo htmlspecialchars($language); ?></oml:language><?php endif; ?>
   <?php if ($licence != null): ?><oml:licence><?php echo htmlspecialchars($licence); ?></oml:licence><?php endif; ?>
   <oml:url><?php echo htmlspecialchars($url); ?></oml:url>
